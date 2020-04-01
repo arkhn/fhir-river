860bcca61c58a3d1ec3506ceb1bf117b1304df36
Andy Barakat <a.barakat@stuart.com>
2020-04-01 18:59:27 +0200

    Fix

--- test/create_file_stfp.py
+++ test/create_file_stfp.py
@@ -4,5 +4,5 @@ Create Records Db
 
 import random
 
-with open('sftp/host/upload/data/csv-sftp-source.csv', "w+") as new_csv:
+with open('sftp/host/upload/data/csv-sftp-{}.csv'.format(int(1000 * random.random())), "w+") as new_csv:
     new_csv.write("andy,corona_{}".format(int(1000 * random.random())))
