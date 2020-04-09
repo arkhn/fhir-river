136bc68959e100aa05e4580c073ddf9dfddd68ec
Jasopaum <jason.paumier@gmail.com>
2020-04-09 13:19:12 +0200

    copy Loader logic from fhir-pipe

--- fhir_loader/src/main.py
+++ fhir_loader/src/main.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 
-import os
-import json
 import datetime
+import json
+import os
+
 from confluent_kafka import KafkaException, KafkaError
-from fhir_loader.src.consumer_class import LoaderConsumer
+
 from fhir_loader.src.config.logger import create_logger
+from fhir_loader.src.consumer_class import LoaderConsumer
 from fhir_loader.src.helper import get_topic_name
+from fhir_loader.src.load import Loader
+from fhir_loader.src.load.fhirstore import get_fhirstore
+
 
 MAX_ERROR_COUNT = 3
-TOPIC = [get_topic_name(source='mimic', resource='Patient', task_type='transform')]
-GROUP_ID = 'arkhn_loader'
+TOPIC = [get_topic_name(source="mimic", resource="Patient", task_type="transform")]
+GROUP_ID = "arkhn_loader"
 
-logging = create_logger('loader')
+logger = create_logger("loader")
 
 
 def process_event(msg):
@@ -23,10 +28,12 @@ def process_event(msg):
     """
     msg_value = json.loads(msg.value())
     msg_topic = msg.topic()
-    logging.info("Loader")
-    logging.info(msg_topic)
-    logging.info(msg_value)
+    logger.info("Loader")
+    logger.info(msg_topic)
+    logger.info(msg_value)
+
     # Load stuff
+    loader.load(fhirstore, msg_value)
 
 
 def manage_kafka_error(msg):
@@ -35,19 +42,25 @@ def manage_kafka_error(msg):
     :param msg:
     :return:
     """
-    logging.error(msg.error())
-    pass
+    logger.error(msg.error())
 
 
-if __name__ == '__main__':
-    logging.info("Running Consumer")
-    consumer = LoaderConsumer(broker=os.getenv("KAFKA_BOOTSTRAP_SERVERS"),
-                              topics=TOPIC,
-                              group_id=GROUP_ID,
-                              process_event=process_event,
-                              manage_error=manage_kafka_error)
+if __name__ == "__main__":
+    logger.info("Running Consumer")
+
+    fhirstore = get_fhirstore()
+
+    # TODO how will we handle bypass_validation in fhir-river?
+    loader = Loader(fhirstore, bypass_validation=True)
+    consumer = LoaderConsumer(
+        broker=os.getenv("KAFKA_BOOTSTRAP_SERVERS"),
+        topics=TOPIC,
+        group_id=GROUP_ID,
+        process_event=process_event,
+        manage_error=manage_kafka_error,
+    )
 
     try:
         consumer.run_consumer()
     except (KafkaException, KafkaError) as err:
-        logging.error(err)
+        logger.error(err)
