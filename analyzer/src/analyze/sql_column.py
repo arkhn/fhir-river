484d5a121ac6593949ea01186cac99f2475c2c64
Jasopaum <jason.paumier@gmail.com>
2020-07-07 16:56:36 +0200

    store truncated name as attribute

--- analyzer/src/analyze/sql_column.py
+++ analyzer/src/analyze/sql_column.py
@@ -18,6 +18,8 @@ class SqlColumn:
         self.cleaning_script = cleaning_script
         self.concept_map = concept_map
 
+        self.truncated_name = None
+
     def __eq__(self, other) -> bool:
         return (
             self.table == other.table and self.column == other.column and self.owner == other.owner
@@ -38,7 +40,7 @@ class SqlColumn:
         else:
             return f"{self.table}"
 
-    def dataframe_column_name(self, index):
+    def dataframe_column_name(self, index=None):
         """ sqlalchemy builds column names as {table}_{column}.
         This method helps retrieving the needed columns from the dataframe.
         """
@@ -48,6 +50,15 @@ class SqlColumn:
 
         # Otherwise, we may have problems with sql not accepting aliases of length > 30
         # so we truncate and replace the end by the hex encoded index
+
+        if self.truncated_name is not None:
+            return self.truncated_name
+
+        if index is None:
+            raise ValueError(
+                "Aliases can't be longer than 30 character, please provide an index argument."
+            )
+
         truncated = name[:-5]
         hex_index = format(index, "5x")
         return f"{truncated}{hex_index}"
