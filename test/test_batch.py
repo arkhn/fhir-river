2e14251600d3d75c06a600fcacc1d1091253ddd4
Jasopaum <jason.paumier@gmail.com>
2020-07-15 11:56:35 +0200

    Avoid relying on staging for integration tests (#77)
    * don't rely on staging for integration tests
    
    * fix python version to 3.7.7
    
    * fix
    
    * fix
    
    * query pyrog in integration test to get resource ids
    
    * add .env.test file
    
    * pull all images
    
    * sleep longer
    
    * uncomment unit tests

--- test/test_batch.py
+++ test/test_batch.py
@@ -3,31 +3,42 @@ import json
 import requests
 
 from .consumer_class import EventConsumer
+from analyzer.src.analyze.graphql import PyrogClient
+
 
 BATCH_SIZE_TOPIC = "batch_size"
 LOAD_TOPIC = "load"
-
-MIMIC_PATIENT_RESOURCE_ID = os.getenv("MIMIC_PATIENT_RESOURCE_ID")
-MIMIC_PRACTITIONER_RESOURCE_ID = os.getenv("MIMIC_PRACTITIONER_RESOURCE_ID")
 
 
 def handle_kafka_error(err):
     raise err
 
 
+def get_resource_ids():
+    sources_query = """
+        query s {
+            sources {
+                id
+                resources {
+                    id
+                }
+            }
+        }
+    """
+    client = PyrogClient()
+    sources_resp = client.run_graphql_query(sources_query)
+
+    return [resource["id"] for resource in sources_resp["data"]["sources"][0]["resources"]]
+
+
 def test_batch_single_row():
     print("START")
 
-    assert (
-        MIMIC_PATIENT_RESOURCE_ID is not None
-    ), "MIMIC_PATIENT_RESOURCE_ID missing from environment"
-    assert (
-        MIMIC_PRACTITIONER_RESOURCE_ID is not None
-    ), "MIMIC_PRACTITIONER_RESOURCE_ID missing from environment"
+    resource_ids = get_resource_ids()
 
     # declare kafka consumer of "load" events
     consumer = EventConsumer(
-        broker=os.getenv("KAFKA_BOOTSTRAP_SERVERS"),
+        broker=os.getenv("KAFKA_BOOTSTRAP_SERVERS_EXTERNAL"),
         topics=LOAD_TOPIC,
         group_id="test_batch_single_row",
         manage_error=handle_kafka_error,
@@ -39,35 +50,26 @@ def test_batch_single_row():
         consumer.run_consumer(event_count=msg_value["size"], poll_timeout=30)
 
     batch_size_consumer = EventConsumer(
-        broker=os.getenv("KAFKA_BOOTSTRAP_SERVERS"),
+        broker=os.getenv("KAFKA_BOOTSTRAP_SERVERS_EXTERNAL"),
         topics=BATCH_SIZE_TOPIC,
         group_id="test_batch_size",
         manage_error=handle_kafka_error,
         process_event=wait_batch,
     )
 
-    # RUN A PATIENT BATCH #
-    try:
-        # send a batch request
-        response = requests.post(
-            "http://localhost:3001/batch", json={"resource_ids": [MIMIC_PATIENT_RESOURCE_ID]},
-        )
-    except requests.exceptions.ConnectionError:
-        raise Exception("Could not connect to the api service")
+    for resource_id in resource_ids:
+        try:
+            # send a batch request
+            response = requests.post(
+                "http://0.0.0.0:3001/batch", json={"resource_ids": [resource_id]},
+            )
+        except requests.exceptions.ConnectionError:
+            raise Exception("Could not connect to the api service")
 
-    assert response.status_code == 200, f"api POST /batch returned an error: {response.text}"
+        assert response.status_code == 200, f"api POST /batch returned an error: {response.text}"
 
-    # RUN A PRACTITIONER BATCH BATCH #
-    response = requests.post(
-        "http://localhost:3001/batch", json={"resource_ids": [MIMIC_PRACTITIONER_RESOURCE_ID]},
-    )
-    assert response.status_code == 200, f"api POST /batch returned an error: {response.text}"
-
-    print("Waiting for a batch_size event...")
-    batch_size_consumer.run_consumer(event_count=1, poll_timeout=30)
-
-    print("Waiting for a batch_size event...")
-    batch_size_consumer.run_consumer(event_count=1, poll_timeout=30)
+        print("Waiting for a batch_size event...")
+        batch_size_consumer.run_consumer(event_count=1, poll_timeout=30)
 
 
 # check in elastic that references have been set
