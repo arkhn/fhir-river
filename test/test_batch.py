2fa511d654b76d1a9411f3573fef29440040f204
Jasopaum <jason.paumier@gmail.com>
2020-07-27 12:05:38 +0200

    test with other topic

--- test/test_batch.py
+++ test/test_batch.py
@@ -7,7 +7,7 @@ from analyzer.src.analyze.graphql import PyrogClient
 
 
 BATCH_SIZE_TOPIC = "batch_size"
-LOAD_TOPIC = "load"
+LOAD_TOPIC = "extract"
 
 
 def handle_kafka_error(err):
@@ -47,7 +47,7 @@ def test_batch_single_row():
     def wait_batch(msg):
         msg_value = json.loads(msg.value())
         print(f"Go batch of size {msg_value['size']}, consuming events...")
-        consumer.run_consumer(event_count=msg_value["size"], poll_timeout=60)
+        consumer.run_consumer(event_count=msg_value["size"], poll_timeout=30)
 
     batch_size_consumer = EventConsumer(
         broker=os.getenv("KAFKA_BOOTSTRAP_SERVERS_EXTERNAL"),
@@ -69,7 +69,7 @@ def test_batch_single_row():
         assert response.status_code == 200, f"api POST /batch returned an error: {response.text}"
 
         print("Waiting for a batch_size event...")
-        batch_size_consumer.run_consumer(event_count=1, poll_timeout=60)
+        batch_size_consumer.run_consumer(event_count=1, poll_timeout=45)
 
 
 # check in elastic that references have been set
