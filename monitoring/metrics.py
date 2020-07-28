ef1bfad482538627e16e7ed1fb06ebdfe9d6b29d
Jasopaum <jason.paumier@gmail.com>
2020-07-28 11:54:07 +0200

    use time method of Histogram instead of importing time

--- monitoring/metrics.py
+++ monitoring/metrics.py
@@ -1,4 +1,3 @@
-import time
 from functools import wraps
 
 from prometheus_client import Histogram
@@ -9,6 +8,7 @@ class Timer:
     Class to be used as a decorator to time functions and store observation in
     a Prometheus histogram.
     """
+
     def __init__(self, *args, **kwargs):
         """
         Takes the same arguments as prometheus_client.Histogram
@@ -18,9 +18,7 @@ class Timer:
     def __call__(self, func, *args, **kwargs):
         @wraps(func)
         def timed(*args, **kwargs):
-            ts = time.time()
-            result = func(*args, **kwargs)
-            self.histogram.observe(time.time() - ts)
-            return result
+            with self.histogram.time():
+                return func(*args, **kwargs)
 
         return timed
