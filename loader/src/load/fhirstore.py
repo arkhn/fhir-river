c7ab912fdeee122c99b6d691a5af7aa8474c51dc
Jasopaum <jason.paumier@gmail.com>
2020-09-10 17:10:11 +0200

    refactor loader's fhirstore

--- loader/src/load/fhirstore.py
+++ loader/src/load/fhirstore.py
@@ -1,12 +1,7 @@
 import os
-from prometheus_client import Counter
 from pymongo import MongoClient
 
 import fhirstore
-from fhir.resources.operationoutcome import OperationOutcome
-
-from loader.src.config.logger import get_logger
-from loader.src.load.utils import get_resource_id
 
 
 FHIRSTORE_HOST = os.getenv("FHIRSTORE_HOST")
@@ -17,13 +12,6 @@ FHIRSTORE_PASSWORD = os.getenv("FHIRSTORE_PASSWORD")
 
 _client = None
 _fhirstore = None
-logger = get_logger()
-
-counter_failed_validations = Counter(
-    "count_failed_validations",
-    "count number of times validation has failed",
-    labelnames=("resource_id",),
-)
 
 
 def get_mongo_client():
@@ -46,39 +34,3 @@ def get_fhirstore():
         if not _fhirstore.initialized:
             _fhirstore.bootstrap()
     return _fhirstore
-
-
-def save_one(fhir_object):
-    """
-    Save instance of FHIR resource in MongoDB through fhirstore.
-
-    args:
-        fhir_object: fhir object to create
-    """
-    store = get_fhirstore()
-
-    resource = store.create(fhir_object)
-    if isinstance(resource, OperationOutcome):
-        resource_id = get_resource_id(fhir_object)
-        # Increment counter for failed validations
-        counter_failed_validations.labels(resource_id=resource_id).inc()
-        # Log
-        logger.error(
-            f"Validation failed for resource {fhir_object}: {resource['issue']}",
-            extra={"resource_id": resource_id},
-        )
-
-
-def get_resource_instances(resource_id, resource_type):
-    global _client
-    store = _client[FHIRSTORE_DATABASE]
-    return store[resource_type].find(
-        {
-            "meta.tag": {
-                "$elemMatch": {
-                    "code": {"$eq": resource_id},
-                    "system": {"$eq": fhirstore.ARKHN_CODE_SYSTEMS.resource},
-                }
-            }
-        }
-    )
--- loader/src/load/loader.py
+++ loader/src/load/loader.py
@@ -1,7 +1,18 @@
-from loader.src.load.fhirstore import save_one
-
 from arkhn_monitoring import Counter, Timer
 from arkhn_monitoring.metrics import FAST_FN_BUCKETS
+from fhir.resources.operationoutcome import OperationOutcome
+
+from loader.src.config.logger import get_logger
+from loader.src.load.utils import get_resource_id
+
+
+logger = get_logger()
+
+counter_failed_validations = Counter(
+    "count_failed_validations",
+    "count number of times validation has failed",
+    labelnames=("resource_id",),
+)
 
 
 class Loader:
@@ -21,4 +32,17 @@ class Loader:
         if resource_type not in self.fhirstore.resources:
             self.fhirstore.bootstrap(resource=resource_type, depth=3)
 
-        save_one(fhir_instance)
+        resource = self.fhirstore.create(fhir_instance)
+        resource_id = get_resource_id(fhir_instance)
+        logger.error(
+            f"After create got: {resource}", extra={"resource_id": resource_id},
+        )
+        if isinstance(resource, OperationOutcome):
+            resource_id = get_resource_id(fhir_instance)
+            # Increment counter for failed validations
+            counter_failed_validations.labels(resource_id=resource_id).inc()
+            # Log
+            logger.error(
+                f"Validation failed for resource {fhir_instance}: {resource['issue']}",
+                extra={"resource_id": resource_id},
+            )
