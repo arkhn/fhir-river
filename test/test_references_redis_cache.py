29bd65bfc0157c963702a2de6b0ea639c44a1c35
Teva RIOU <teva.riou@gmail.com>
2020-09-15 17:58:25 +0200

    feat: add refs cache integration test

---
+++ test/test_references_redis_cache.py
@@ -0,0 +1,78 @@
+import os
+import json
+import requests
+
+from .consumer_class import EventConsumer
+from analyzer.src.analyze.graphql import PyrogClient
+from loader.src.cache import redis
+
+BATCH_SIZE_TOPIC = "batch_size"
+LOAD_TOPIC = "load"
+
+
+def handle_kafka_error(err):
+    raise err
+
+
+def get_resource_ids():
+    sources_query = """
+        query s {
+            sources {
+                id
+                resources {
+                    id
+                }
+            }
+        }
+    """
+    client = PyrogClient()
+    sources_resp = client.run_graphql_query(sources_query)
+
+    return [resource["id"] for resource in sources_resp["data"]["sources"][0]["resources"]]
+
+
+def test_batch_single_row():
+    print("START")
+
+    resource_ids = get_resource_ids()
+    r = redis.conn()
+
+    # declare kafka consumer of "load" events
+    consumer = EventConsumer(
+        broker=os.getenv("KAFKA_BOOTSTRAP_SERVERS_EXTERNAL"),
+        topics=LOAD_TOPIC,
+        group_id="test_batch_single_row",
+        manage_error=handle_kafka_error,
+    )
+
+    def wait_batch(msg):
+        msg_value = json.loads(msg.value())
+        print(f"Go batch of size {msg_value['size']}, consuming events...")
+        consumer.run_consumer(event_count=msg_value["size"], poll_timeout=15)
+
+    batch_size_consumer = EventConsumer(
+        broker=os.getenv("KAFKA_BOOTSTRAP_SERVERS_EXTERNAL"),
+        topics=BATCH_SIZE_TOPIC,
+        group_id="test_batch_size",
+        manage_error=handle_kafka_error,
+        process_event=wait_batch,
+    )
+
+    for resource_id in resource_ids:
+        try:
+            # send a batch request
+            response = requests.post(
+                "http://0.0.0.0:3001/batch", json={"resource_ids": [resource_id]},
+            )
+        except requests.exceptions.ConnectionError:
+            raise Exception("Could not connect to the api service")
+
+        assert response.status_code == 200, f"api POST /batch returned an error: {response.text}"
+
+        print("Waiting for a batch_size event...")
+        batch_size_consumer.run_consumer(event_count=1, poll_timeout=15)
+
+    # Check if references cache is empty
+    assert r.dbsize() == 0
+
+# check in elastic that references have been set
