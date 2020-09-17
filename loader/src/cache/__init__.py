76965fdfa0a65e8d5e25a556f6683cc1b19e4743
Teva RIOU <teva.riou@gmail.com>
2020-09-17 15:46:15 +0200

    feat: create a new redis instance for loader

--- loader/src/cache/__init__.py
+++ loader/src/cache/__init__.py
@@ -2,7 +2,7 @@ import os
 from loader.src.config.service_logger import get_logger
 
 
-REDIS_HOST = os.getenv("REDIS_HOST")
-REDIS_PORT = os.getenv("REDIS_PORT")
-REDIS_DB = os.getenv("REDIS_DB")
+REDIS_HOST = os.getenv("LOADER_REDIS_HOST")
+REDIS_PORT = os.getenv("LOADER_REDIS_PORT")
+REDIS_DB = os.getenv("LOADER_REDIS_DB")
 logger = get_logger()
