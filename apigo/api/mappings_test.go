7dbd4be199f8d542de57377703a4a676825176ad
Jasopaum <jason.paumier@gmail.com>
2020-10-14 14:31:54 +0200

    add unit tests for fetchConceptMap in apigo

--- apigo/api/mappings_test.go
+++ apigo/api/mappings_test.go
@@ -73,10 +73,65 @@ func TestFetchMapping(t *testing.T) {
 	})
 }
 
-func TestDereferenceConceptMap(t *testing.T) {
+func TestFetchConceptMap(t *testing.T) {
+	// Mock fhir api
+	fhirConceptMap := `{
+		"id": "cm_gender",
+		"group": [{"element": [
+			{"code": "F", "target": [{"code": "female", "equivalence": "equal"}]},
+			{"code": "M", "target": [{"code": "male", "equivalence": "equal"}]}
+		]}]
+	}`
+	mockFhirApi := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authorizationHeader := r.Header.Get("Authorization")
+		if authorizationHeader == "Bearer validToken" {
+			fmt.Fprint(w, fhirConceptMap)
+		} else if authorizationHeader == "Bearer forbiddenToken" {
+			http.Error(w, "invalid token", http.StatusForbidden)
+		} else {
+			http.Error(w, "invalid token", http.StatusUnauthorized)
+		}
+	}))
+	defer mockFhirApi.Close()
+	fhirURL = mockFhirApi.URL
 
+	t.Run("fetched map should be correct", func(t *testing.T) {
+		conceptMap, err := fetchConceptMap("mapID", "Bearer validToken")
+		assert.NoError(t, err)
+
+		m := make(map[string]string)
+		m["F"] = "female"
+		m["M"] = "male"
+		assert.Equal(t, m, conceptMap, "concept map is incorrect")
+	})
+
+	t.Run("query api without enough rights", func(t *testing.T) {
+		_, err := fetchConceptMap("resourceID", "Bearer forbiddenToken")
+		if err == nil {
+			t.Fatal("expected an error")
+		}
+
+		got, isInvalidTokenError := err.(*invalidTokenError)
+		if !isInvalidTokenError {
+			t.Fatalf("expected an isInvalidTokenError, got %v", err)
+		}
+		assert.Equal(t, http.StatusForbidden, got.statusCode, "status code is incorrect")
+	})
+
+	t.Run("query api with invalid token", func(t *testing.T) {
+		_, err := fetchConceptMap("resourceID", "Bearer invalidToken")
+		if err == nil {
+			t.Fatal("expected an error")
+		}
+
+		got, isInvalidTokenError := err.(*invalidTokenError)
+		if !isInvalidTokenError {
+			t.Fatalf("expected an isInvalidTokenError, got %v", err)
+		}
+		assert.Equal(t, http.StatusUnauthorized, got.statusCode, "status code is incorrect")
+	})
 }
 
-func TestFetchConceptMap(t *testing.T) {
+func TestDereferenceConceptMap(t *testing.T) {
 
 }
