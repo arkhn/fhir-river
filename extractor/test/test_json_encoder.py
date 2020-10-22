8867f49bb47ce1ebabd4c7f86e480ef934a93b6d
Jasopaum <jason.paumier@gmail.com>
2020-10-22 10:54:14 +0200

    fix JSONEncoder tests

--- extractor/test/test_json_encoder.py
+++ extractor/test/test_json_encoder.py
@@ -1,16 +1,20 @@
+from datetime import date, datetime
 from decimal import Decimal
 
 from extractor.src.json_encoder import MyJSONEncoder
 
 
 def test_encode_decimal():
-    json_encoder = MyJSONEncoder()
+    encoded = MyJSONEncoder().default(Decimal("5.0"))
+    assert encoded == 5.0
 
-    encoded = json_encoder.default(Decimal("5.0"))
-    assert encoded == "5"
+    encoded = MyJSONEncoder().default(Decimal("5"))
+    assert encoded == 5.0
 
-    encoded = json_encoder.default(Decimal("5"))
-    assert encoded == "5"
 
-    encoded = json_encoder.default(Decimal("5.1030000"))
-    assert encoded == "5.103"
+def test_encode_datetime():
+    encoded = MyJSONEncoder().default(date(2020, 10, 10))
+    assert encoded == "2020-10-10"
+
+    encoded = MyJSONEncoder().default(datetime(2020, 10, 10, 12, 12, 12))
+    assert encoded == "2020-10-10T12:12:12"
