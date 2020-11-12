ff76711db3a2712121d06a156ad55ee78d663507
simonvadee <simon.vadee@gmail.com>
2020-11-12 18:54:15 +0100

    Remove integration tests

--- test/utils/create_file_stfp.py
+++
@@ -1,8 +0,0 @@
-"""
-Create Records Db
-"""
-
-import random
-
-with open('sftp/host/upload/data/csv-sftp-{}.csv'.format(int(1000 * random.random())), "w+") as new_csv:
-    new_csv.write("andy,corona_{}".format(int(1000 * random.random())))
