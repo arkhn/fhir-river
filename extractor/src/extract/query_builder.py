a85a020174542ffecfb65fb0d2842f9219cd9204
Jasopaum <jason.paumier@gmail.com>
2020-11-24 11:25:17 +0100

    keep a dict of joins to avoid duplicating them

--- extractor/src/extract/query_builder.py
+++ extractor/src/extract/query_builder.py
@@ -59,6 +59,9 @@ class QueryBuilder:
         # so we keep the one we've already seen here.
         self._condition_columns = set()
 
+        # To avoid duplciated joins, we keep them here
+        self._cur_query_join_tables = {}
+
     @Timer("time_extractor_build_query", "time to build sql query")
     def build_query(self) -> Query:
         """ Builds an sql alchemy query which will be run in run_sql_query.
@@ -67,6 +70,10 @@ class QueryBuilder:
             f"Start building query for resource {self.analysis.definition_id}",
             extra={"resource_id": self.analysis.resource_id},
         )
+
+        # Check that temp attributes are empty
+        self._condition_columns = set()
+        self._cur_query_join_tables = {}
 
         query = self.session.query(
             self.get_column(self.analysis.primary_key_column, self._sqlalchemy_pk_table)
@@ -88,20 +95,24 @@ class QueryBuilder:
     def add_attribute_to_query(self, query: Query, attribute: Attribute):
         for input_group in attribute.input_groups:
             for col in input_group.columns:
-                # Add the column to select to the query
-                sqlalchemy_table = self.get_table(col)
-                sqlalchemy_col = self.get_column(col, sqlalchemy_table)
-                query = query.add_columns(sqlalchemy_col)
 
                 # Apply joins to the query
                 # We keep the used join tables in a dict in case we have multi-hop joins
-                join_tables = {col.table: sqlalchemy_table}
+                join_tables = {}
                 for join in col.joins:
+                    if join in self._cur_query_join_tables:
+                        # This join was already added to the query
+                        continue
+
                     # Get tables
                     right_table = join_tables.get(join.right.table, self.get_table(join.right))
-                    join_tables[join.right.table] = right_table
                     left_table = join_tables.get(join.left.table, self.get_table(join.left))
+                    join_tables[join.right.table] = right_table
                     join_tables[join.left.table] = left_table
+
+                    # Add the join to the temp join dict
+                    self._cur_query_join_tables[join] = right_table
+
                     # Add join
                     query = query.join(
                         right_table,
@@ -109,6 +120,16 @@ class QueryBuilder:
                         == self.get_column(join.left, left_table),
                         isouter=True,
                     )
+
+                # Add the column to select to the query
+                if col.joins:
+                    # The last join will be the one on the input table
+                    sqlalchemy_table = self._cur_query_join_tables[col.joins[-1]]
+                else:
+                    with_alias = len(col.joins) > 0
+                    sqlalchemy_table = self.get_table(col, with_alias=with_alias)
+                sqlalchemy_col = self.get_column(col, sqlalchemy_table)
+                query = query.add_columns(sqlalchemy_col)
 
             # Add the condition columns to the query
             for condition in input_group.conditions:
