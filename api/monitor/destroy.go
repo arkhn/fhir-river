8765792abf163d89c8ebaab5f1f7496f6139fed5
Teva RIOU <teva.riou@gmail.com>
2020-12-01 17:15:18 +0100

    feat: expire counter key in Redis instead of deleting it

--- api/monitor/destroy.go
+++ api/monitor/destroy.go
@@ -11,7 +11,11 @@ import (
 
 // Destroy deletes a batch by removing its Redis keys and Kafka topics
 func (ctl BatchController) Destroy(batchID string) error {
-	if _, err := ctl.Redis().Del("batch:"+batchID+":counter").Result(); err != nil {
+	twoWeeks, err := time.ParseDuration("336h")
+	if err != nil {
+		return err
+	}
+	if _, err := ctl.Redis().Expire("batch:"+batchID+":counter", twoWeeks).Result(); err != nil {
 		return err
 	}
 	if _, err := ctl.Redis().Del("batch:"+batchID+":resources").Result(); err != nil {
