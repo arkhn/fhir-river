6f2737f24b0276bc94bf276e6a512451a7cde1b9
Teva RIOU <teva.riou@gmail.com>
2020-12-01 09:53:21 +0100

    fix: tests by putting patient mapping json in a string literal

--- api/mocks/pyrog_server.go
+++ api/mocks/pyrog_server.go
@@ -2,17 +2,12 @@ package mocks
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 )
 
 func PyrogServer() *httptest.Server {
-	mappingBytes, err := ioutil.ReadFile("../../../analyzer/test/fixtures/patient_mapping.json")
-	if err != nil {
-		panic(err)
-	}
-	pyrogResponse := fmt.Sprintf(`{"data": {"resource": %s}}`, string(mappingBytes))
+	pyrogResponse := fmt.Sprintf(`{"data": {"resource": %s}}`, patientMapping)
 	mockPyrogServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authorizationHeader := r.Header.Get("Authorization")
 		if authorizationHeader == "Bearer validToken" {
