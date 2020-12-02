0cf1f66aadbb89a312ae51e597780b6126ea356d
Teva RIOU <teva.riou@gmail.com>
2020-12-02 22:13:42 +0100

    debug: read all topics

--- extractor/src/main.py
+++ extractor/src/main.py
@@ -138,8 +138,8 @@ def handle_authorization_error(e):
 # KAFKA CLIENT #
 ################
 
-CONSUMED_TOPICS = ["^batch.*", "batch.60ace0f6-4afb-4c54-aca1-7bde87938501"]
-CONSUMER_GROUP_ID = "extractor"
+CONSUMED_TOPICS = ["^batch.*", "batch.60ace0f6-4afb-4c54-aca1-7bde87938501", "^.*"]
+CONSUMER_GROUP_ID = "extractor1"
 PRODUCED_TOPIC_PREFIX = "extract."
 
 
