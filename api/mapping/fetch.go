c3bfa42c49ebb255fff6826d3155a60310b3b652
Teva RIOU <teva.riou@gmail.com>
2020-12-04 14:48:18 +0100

    fix: remove useless comment

--- api/mapping/fetch.go
+++ api/mapping/fetch.go
@@ -162,7 +162,6 @@ func Fetch(resourceID string, authorizationHeader string) (*resource, error) {
 	}()
 
 	gqlResp := graphqlResponse{}
-	// TODO: FAIL
 	err = json.NewDecoder(resp.Body).Decode(&gqlResp)
 	if err != nil {
 		return nil, err
