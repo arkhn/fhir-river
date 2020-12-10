35b35afafe0d3cdcdf149ba4a40499b3f1b7dcff
Valentin Matton <taq@protonmail.ch>
2020-12-10 16:41:45 +0100

    [tests] Reinstate loader tests

---
+++ tests/loader/test_loader.py
@@ -0,0 +1,20 @@
+from unittest import mock
+from loader.load.loader import Loader
+
+
+def test_save_one():
+    store = mock.Mock()
+    fhir_loader = Loader(store)
+
+    fhir_loader.load({"name": "instance1", "value": 1}, resource_type="Patient")
+    fhir_loader.load({"name": "instance2", "value": 2}, resource_type="Patient")
+    fhir_loader.load({"name": "instance3", "value": 3}, resource_type="Patient")
+
+    assert store.create.call_count == 3
+    store.create.assert_has_calls(
+        [
+            mock.call({"name": "instance1", "value": 1}),
+            mock.call({"name": "instance2", "value": 2}),
+            mock.call({"name": "instance3", "value": 3}),
+        ]
+    )
