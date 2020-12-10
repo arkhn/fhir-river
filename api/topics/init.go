76db51b724f53f897a340f04cc8a3334d66b8323
Teva RIOU <teva.riou@gmail.com>
2020-12-10 14:07:11 +0100

    feat: add num parts as env var

--- api/topics/init.go
+++ api/topics/init.go
@@ -1,14 +1,32 @@
 package topics
 
+import (
+	"os"
+	"strconv"
+)
+
 const (
 	BatchPrefix       = "batch."
 	ExtractPrefix     = "extract."
 	TransformPrefix   = "transform."
 	LoadPrefix        = "load."
-	NumParts          = 1
 	ReplicationFactor = 1
 	Batch             = "^batch\\..*"
 	Extract           = "^extract\\..*"
 	Transform         = "^transform\\..*"
 	Load              = "^load\\..*"
 )
+
+var NumParts int64
+
+func init() {
+	numPartsEnv, isNumPartsEnv := os.LookupEnv("KAFKA_NUMBER_PARTITIONS")
+	if !isNumPartsEnv {
+		NumParts = 1
+	} else {
+		var err error
+		if NumParts, err = strconv.ParseInt(numPartsEnv, 10, 32); err != nil {
+			panic(err)
+		}
+	}
+}
