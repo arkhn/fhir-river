b8fc14fd543336a090e7a160e44b2b3c9efc52a0
Jasopaum <jason.paumier@gmail.com>
2020-12-22 10:47:02 +0100

    [extractor] unit tests for joins on filters

--- extractor/test/extract/test_query_builder.py
+++ extractor/test/extract/test_query_builder.py
@@ -105,7 +105,7 @@ def test_sqlalchemy_query(mock_sha1):
                 ],
             ),
             "LIKE",
-            "'2150-08-29'",
+            "2150-08-29",
         )
     )
     analysis.attributes.append(attributeA)
@@ -123,6 +123,7 @@ def test_sqlalchemy_query(mock_sha1):
         "LEFT OUTER JOIN admissions AS admissions_1 ON admissions_1.row_id = patients.row_id \n"
         "WHERE admissions_1.admittime LIKE :param_1"
     )
+    assert query.statement.compile().params == {"param_1": "2150-08-29"}
 
 
 @mock.patch("analyzer.src.analyze.sql_column.hashlib.sha1")
@@ -243,38 +244,44 @@ def test_duplicated_joins(mock_sha1):
     )
 
 
+@mock.patch("analyzer.src.analyze.sql_column.hashlib.sha1")
 @mock.patch("extractor.src.extract.query_builder.Table", mock_table)
-def test_apply_filters():
+def test_apply_filters(mock_sha1):
+    mock_sha1.return_value.hexdigest.return_value = "hash"
     analysis = Analysis()
 
     analysis.primary_key_column = SqlColumn("patients", "subject_id")
-    analysis.add_filter(SqlFilter(SqlColumn("admissions", "admittime"), "LIKE", "'2150-08-29'"))
+    analysis.add_filter(SqlFilter(SqlColumn("admissions", "admittime"), "LIKE", "2150-08-29"))
     analysis.add_filter(SqlFilter(SqlColumn("patients", "row_id"), "<=", "1000"))
     analysis.add_filter(SqlFilter(SqlColumn("patients", "row_id"), "BETWEEN", "500,800"))
 
     pk_values = [123, 456]
 
     query_builder = make_query_builder(analysis, pk_values)
+    query = query_builder.build_query()
 
-    base_query = mock.MagicMock()
-    base_query.filter.return_value = base_query
+    assert str(query) == (
+        "SELECT patients.subject_id AS patients_subject_id_hash \n"
+        "FROM patients, admissions \n"
+        "WHERE patients.subject_id IN (:param_1, :param_2) "
+        "AND admissions.admittime LIKE :param_3 "
+        "AND patients.row_id <= :param_4 "
+        "AND patients.row_id >= :param_5 AND patients.row_id <= :param_6"
+    )
+    assert query.statement.compile().params == {
+        "param_1": 123,
+        "param_2": 456,
+        "param_3": "2150-08-29",
+        "param_4": "1000",
+        "param_5": "500",
+        "param_6": "800",
+    }
 
-    query_builder.apply_filters(base_query)
 
-    binary_expressions = [
-        "patients.subject_id IN (:param_1, :param_2)",
-        "admissions.admittime LIKE :param_1",
-        "patients.row_id <= :param_1",
-        "patients.row_id >= :param_1 AND patients.row_id <= :param_2",
-    ]
-
-    for call, binary_expression in zip(base_query.filter.call_args_list, binary_expressions):
-        args, _ = call
-        assert str(args[0]) == binary_expression
-
-
+@mock.patch("analyzer.src.analyze.sql_column.hashlib.sha1")
 @mock.patch("extractor.src.extract.query_builder.Table", mock_table)
-def test_apply_filters_single_value():
+def test_apply_filters_single_value(mock_sha1):
+    mock_sha1.return_value.hexdigest.return_value = "hash"
     analysis = Analysis()
     analysis.primary_key_column = SqlColumn("patients", "subject_id")
 
@@ -282,16 +289,52 @@ def test_apply_filters_single_value():
     filter_values = [primary_key]
 
     query_builder = make_query_builder(analysis, filter_values)
+    query = query_builder.build_query()
 
-    base_query = mock.MagicMock()
-    base_query.filter.return_value = base_query
+    assert str(query) == (
+        "SELECT patients.subject_id AS patients_subject_id_hash \n"
+        "FROM patients \n"
+        "WHERE patients.subject_id = :param_1"
+    )
+    assert query.statement.compile().params == {"param_1": 123}
 
-    query_builder.apply_filters(base_query)
 
-    binary_expressions = [
-        query_builder.get_column(SqlColumn("patients", "subject_id")).__eq__(primary_key),
-    ]
+@mock.patch("analyzer.src.analyze.sql_column.hashlib.sha1")
+@mock.patch("extractor.src.extract.query_builder.Table", mock_table)
+def test_filters_with_joins(mock_sha1):
+    mock_sha1.return_value.hexdigest.return_value = "hash"
+    analysis = Analysis()
 
-    for call, binary_expression in zip(base_query.filter.call_args_list, binary_expressions):
-        args, _ = call
-        assert str(args[0]) == str(binary_expression)
+    analysis.primary_key_column = SqlColumn("patients", "subject_id")
+    analysis.add_filter(
+        SqlFilter(
+            SqlColumn(
+                "admissions",
+                "admittime",
+                joins=[
+                    SqlJoin(
+                        SqlColumn("patients", "subject_id"), SqlColumn("admissions", "subject_id")
+                    )
+                ],
+            ),
+            "LIKE",
+            "2150-08-29",
+        )
+    )
+
+    query_builder = make_query_builder(analysis)
+    query = query_builder.build_query()
+
+    assert str(query) == (
+        "SELECT patients.subject_id AS patients_subject_id_hash \n"
+        "FROM patients LEFT OUTER JOIN admissions AS admissions_1 "
+        "ON admissions_1.subject_id = patients.subject_id \n"
+        "WHERE admissions_1.admittime LIKE :param_1"
+    )
+    assert query.statement.compile().params == {"param_1": "2150-08-29"}
+
+
+@mock.patch("extractor.src.extract.query_builder.Table", mock_table)
+def test_conditions_with_joins():
+    # TODO
+    pass
