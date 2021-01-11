7255335761cbb30a8795cd64b7329b273ee4bfc4
Valentin Matton <taq@protonmail.ch>
2021-01-11 10:03:47 +0100

    [django] Disable admin

--- django/river/urls.py
+++ django/river/urls.py
@@ -5,7 +5,6 @@ from core import urls as core_urls
 from preview import urls as preview_urls
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("api/core/", include(core_urls)),
     path("api/", include(preview_urls)),
 ]
