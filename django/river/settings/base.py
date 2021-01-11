06c0a73b1a5f3e0b3246e785f09233b497f8e440
Valentin Matton <taq@protonmail.ch>
2021-01-11 16:13:30 +0100

    [settings] Fix base dir

--- django/river/settings/base.py
+++ django/river/settings/base.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # API versioning
 
