eb84fcd735ae0dd6d9c0846cb97b7b685a184a21
Teva RIOU <teva.riou@gmail.com>
2021-01-14 18:16:56 +0100

    refactor: naming

--- api/routes/batch/list.go
+++ api/routes/batch/list.go
@@ -9,16 +9,16 @@ import (
 
 func List(ctl monitor.BatchController) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var response []Response
+		var responseList []Response
 		list, err := ctl.BatchList()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		for id, timestamp := range list {
-			response = append(response, Response{Id: id, Timestamp: timestamp})
+			responseList = append(responseList, Response{Id: id, Timestamp: timestamp})
 		}
-		if err := json.NewEncoder(w).Encode(response); err != nil {
+		if err := json.NewEncoder(w).Encode(responseList); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	}
