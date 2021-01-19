92d3cadae4e69278f86556ff39e2b75d98b5c2a1
triou <34629112+tevariou@users.noreply.github.com>
2021-01-19 16:24:40 +0100

    refactor: normalize batch response data (#217)
    * refactor: normalize batch response data from api endpoints

--- api/topics/monitor/batch.go
+++ api/topics/monitor/batch.go
@@ -1,10 +1,8 @@
 package monitor
 
-import "time"
-
 // BatchSet records a batch in Redis
-func (ctl BatchController) BatchSet(batchID string) error {
-	return ctl.rdb.HSet("batch", batchID, time.Now().Format(time.RFC3339)).Err()
+func (ctl BatchController) BatchSet(batchID string, timestamp string) error {
+	return ctl.rdb.HSet("batch", batchID, timestamp).Err()
 }
 
 // BatchList returns a list of the current batches
