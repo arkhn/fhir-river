2ad525719de7a62825149dcf6c3c94375b02ea35
Valentin Matton <taq@protonmail.ch>
2021-01-20 16:33:45 +0100

    [monitoring] Start exporter with services

--- django/loader/management/commands/loader.py
+++ django/loader/management/commands/loader.py
@@ -3,13 +3,15 @@ import logging
 from django.core.management.base import BaseCommand
 
 from loader.service import LoaderService
+from utils.exporter import start_exporter
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        logger.info("Starting...")
+        start_exporter()
 
+        logger.info("Starting...")
         app = LoaderService.make_app()
         app.run()
