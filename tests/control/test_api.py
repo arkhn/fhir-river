0b1275721d26cd152801dbbd10bb3e47bae67907
Simon Vadée <simon.vadee@gmail.com>
2021-01-22 15:32:56 +0100

    Move cleaning-scripts api to river (#233)
    * [api] expose a route to list scripts
    
    * [tests] unit test of script view
    
    * [control-api] fix destructuring in scripts api

--- tests/control/test_api.py
+++ tests/control/test_api.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 # flake8: noqa
 from rest_framework.test import APIClient
 
@@ -23,3 +25,26 @@ def test_delete_batch_endpoint(api_client: APIClient):
     data = {"resources": []}
     response = api_client.post(url, data=data, format="json")
     # assert response.status_code == 200
+
+
+@mock.patch("control.api.views.getmembers")
+@mock.patch("control.api.views.getdoc")
+def test_list_scripts_endpoint(mock_getdoc, mock_getmembers, api_client: APIClient):
+    mock_getmembers.side_effect = [
+        [("module1", None), ("module2", None)],
+        [("script1", None), ("script2", None)],
+        [("script3", None), ("script4", None)],
+    ]
+    mock_getdoc.return_value = "description"
+
+    url = reverse("scripts")
+    response = api_client.get(url)
+    assert response.status_code == 200
+
+    script_list = response.json()
+    assert script_list == [
+        {"category": "module1", "description": "description", "name": "script1"},
+        {"category": "module1", "description": "description", "name": "script2"},
+        {"category": "module2", "description": "description", "name": "script3"},
+        {"category": "module2", "description": "description", "name": "script4"},
+    ]
