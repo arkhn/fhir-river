5ed0577e7ae0c8e7e1abad15463e7b4669eaa05c
Teva RIOU <teva.riou@gmail.com>
2021-02-01 23:43:50 +0100

    test: broadcast_events when no exception occurs

--- tests/extractor/extract/test_broadcast_events.py
+++ tests/extractor/extract/test_broadcast_events.py
@@ -23,17 +23,31 @@ class BroadCastEventsTestCase(unittest.TestCase):
 
     @mock.patch("common.kafka.producer.Producer", return_value=mock.MagicMock())
     @mock.patch("extractor.extract.extractor.Extractor", return_value=mock.MagicMock())
-    def test_calling_redis_when_batch_empty(self, mock_extractor, mock_producer):
+    def test_redis_counter_when_batch_empty(self, mock_extractor, mock_producer):
         extractor = mock_extractor()
-        extractor.split_dataframe.return_value = [1, 2, 3]
+        extractor.split_dataframe.side_effect = EmptyResult
         producer = mock_producer()
-        producer.produce_event.side_effect = EmptyResult
         analysis = mock.Mock(definition_id='definition_id', resource_id='resource_id')
         redis = mock.MagicMock()
 
-        broadcast_events(None, analysis, producer, extractor, redis)
+        broadcast_events(None, analysis, producer, extractor, redis, "batch_id")
 
         redis.hset.assert_called_once()
+        redis.hset.assert_called_with("batch:batch_id:counter", "resource:resource_id:extracted", 0)
+
+    @mock.patch("common.kafka.producer.Producer", return_value=mock.MagicMock())
+    @mock.patch("extractor.extract.extractor.Extractor", return_value=mock.MagicMock())
+    def test_redis_counter_when_no_exception_occurs(self, mock_extractor, mock_producer):
+        extractor = mock_extractor()
+        extractor.split_dataframe.return_value = [1, 2, 3]
+        producer = mock_producer()
+        analysis = mock.Mock(definition_id='definition_id', resource_id='resource_id')
+        redis = mock.MagicMock()
+
+        broadcast_events(None, analysis, producer, extractor, redis, "batch_id")
+
+        redis.hset.assert_called_once()
+        redis.hset.assert_called_with("batch:batch_id:counter", "resource:resource_id:extracted", 3)
 
 
 if __name__ == '__main__':
