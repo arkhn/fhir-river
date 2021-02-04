5461c5dd8e93bac1e7127c339776ec64bb88ae6e
Jasopaum <jason.paumier@gmail.com>
2021-02-04 11:44:43 +0100

    [tests] Test fetch_concept_map with valid token

--- tests/control/test_fetch_mapping.py
+++ tests/control/test_fetch_mapping.py
@@ -1,6 +1,10 @@
+import json
+from pathlib import Path
 from unittest import mock
 
-from control.api.fetch_mapping import dereference_concept_map
+from control.api import fetch_mapping
+
+FIXTURES_DIR = Path(__file__).resolve().parent / "fixtures"
 
 fhirConceptMap = {
     "id": "cm_gender",
@@ -33,11 +37,33 @@ def mocked_fhir_api_get(url, headers):
         return MockResponse("invalid token", 401)
 
 
+def mocked_pyrog_api_get(url, headers):
+    if headers["Authorization"] == "Bearer validToken":
+        with open(FIXTURES_DIR / "patient_mapping.json", "r") as fp:
+            return MockResponse({"data": {"resource": json.load(fp)}}, 200)
+    elif headers["Authorization"] == "Bearer forbiddenToken":
+        return MockResponse({"errors": [{"statusCode": 403, "message": "forbidden"}]}, 200)
+    elif headers["Authorization"] == "Bearer unauthorizedToken":
+        return MockResponse({"errors": [{"statusCode": 401, "message": "unauthorized"}]}, 200)
+    else:
+        return MockResponse("invalid token", 400)
+
+
+@mock.patch("control.api.fetch_mapping.requests.get", side_effect=mocked_fhir_api_get)
+def test_fetch_concept_map(_):
+    actual = fetch_mapping.fetch_concept_map("id", "Bearer validToken")
+    expected = {
+        "F": "female",
+        "M": "male",
+    }
+    assert actual == expected
+
+
 @mock.patch("control.api.fetch_mapping.requests.get", side_effect=mocked_fhir_api_get)
 def test_dereference_concept_map(_):
 
     mapping = {"attributes": [{"inputGroups": [{"inputs": [{"conceptMapId": "cm_gender"}]}]}]}
 
-    dereference_concept_map(mapping, "Bearer validToken")
+    fetch_mapping.dereference_concept_map(mapping, "Bearer validToken")
 
     assert mapping["attributes"][0]["inputGroups"][0]["inputs"][0]["conceptMap"] == {"F": "female", "M": "male"}
