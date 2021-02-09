3f9d952809a40f504634a241a9da06e53b4b1119
Valentin Matton <taq@protonmail.ch>
2021-02-09 11:37:07 +0100

    [tests] Add resource endpoint

--- tests/pyrog/factories.py
+++ tests/pyrog/factories.py
@@ -6,3 +6,26 @@ class SourceFactory(factory.django.DjangoModelFactory):
         model = "pyrog.Source"
 
     name = factory.Sequence(lambda n: f"source_{n}")
+
+
+class ResourceFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "pyrog.Resource"
+
+    source = factory.SubFactory(SourceFactory)
+    primary_key_owner = factory.SubFactory("tests.pyrog.factories.OwnerFactory")
+
+
+class CredentialFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "pyrog.Credential"
+
+    source = factory.SubFactory(SourceFactory)
+    port = 5432
+
+
+class OwnerFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "pyrog.Owner"
+
+    credential = factory.SubFactory(CredentialFactory)
