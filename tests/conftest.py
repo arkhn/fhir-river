db8b53da1687bc25e160cb4b1ed33f83221461cd
Valentin Matton <taq@protonmail.ch>
2021-02-11 15:43:45 +0100

    [tests] Load env with bootstrap hook

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,4 @@
-import os
-
 import pytest
-from dotenv import find_dotenv, load_dotenv
-
-import django
-
-
-def pytest_configure():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "river.settings.dev")
-    load_dotenv(find_dotenv())
-
-    django.setup()
 
 
 @pytest.fixture
