ad8c80b9f9039f7ce1d94580decc4d685eaa2464
BPierrick <pierrick.bellut95@gmail.com>
2021-02-17 14:15:31 +0100

    Initialize internationalization env

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app/App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app/App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+
+import "./services/internationalization/i18n";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,7 +14,7 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
