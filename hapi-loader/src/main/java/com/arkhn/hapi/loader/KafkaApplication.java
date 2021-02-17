d57fb6a82ef14ea2d65e1a23b786fa4c31a091ba
simonvadee <simon.vadee@gmail.com>
2021-02-17 15:27:49 +0100

    [hapi-loader] wire hapi-fhir-jpa beans to kafka consumer

--- hapi-loader/src/main/java/com/arkhn/hapi/loader/KafkaApplication.java
+++ hapi-loader/src/main/java/com/arkhn/hapi/loader/KafkaApplication.java
@@ -1,5 +1,15 @@
 package com.arkhn.hapi.loader;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +22,20 @@ import org.springframework.core.annotation.AliasFor;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.transaction.support.TransactionCallback;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.interceptor.api.Pointcut;
+import ca.uhn.fhir.jpa.api.config.DaoConfig;
+import ca.uhn.fhir.jpa.api.dao.DaoRegistry;
+import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
+import ca.uhn.fhir.jpa.api.dao.IFhirSystemDao;
+import ca.uhn.fhir.jpa.dao.tx.HapiTransactionService;
+import ca.uhn.fhir.jpa.provider.IJpaSystemProvider;
+import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.rest.api.server.storage.TransactionDetails;
+import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
+import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
+import ca.uhn.fhir.rest.server.provider.ResourceProviderFactory;
 
 @SpringBootApplication
 public class KafkaApplication {
@@ -27,6 +51,15 @@ public class KafkaApplication {
 
     @KafkaListener(id = "loader", topics = "${message.topic.name}", containerFactory = "kafkaListenerContainerFactory", autoStartup = "true", concurrency = "4")
     public static class MessageListener {
+
+        @Autowired
+        DaoRegistry daoRegistry;
+
+        @Autowired
+        FhirContext myFhirContext;
+
+        @Autowired
+        private HapiTransactionService myHapiTransactionService;
 
         private static final Logger log = LoggerFactory.getLogger(MessageListener.class);
 
