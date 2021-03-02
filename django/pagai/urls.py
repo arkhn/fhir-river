e69f4534ee4a42aaf7b04ecb2a1601364a3871e1
Teva RIOU <teva.riou@gmail.com>
2021-03-02 17:29:36 +0100

    refactor(pagai): routes

--- django/pagai/urls.py
+++ django/pagai/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from pagai.api import views
 
 urlpatterns = [
-    path("pagai/list-owners/", views.OwnersListView.as_view(), name="list-owners"),
-    path("pagai/owner-schema/<owner>/", views.OwnerSchemaView.as_view(), name="owner-schema"),
+    path("pagai/owners/", views.OwnersListView.as_view(), name="list-owners"),
+    path("pagai/owners/<owner>/schema/", views.OwnerSchemaView.as_view(), name="owner-schema"),
     path("pagai/explore/<resource_id>/<owner>/<table>/", views.ExploreView.as_view(), name="explore"),
 ]
