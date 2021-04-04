e1d0fcfe05abfb0dcc7d3dbed65dab71e7525b07
Teva Riou <teva.riou@gmail.com>
2021-04-04 18:39:01 +0000

    feat(app): demo uncontrolled react hook form

--- app/src/features/credentials/CredentialForm.tsx
+++ app/src/features/credentials/CredentialForm.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-import Form from "@arkhn/ui/lib/Form/Form";
-import { FormInputProperty } from "@arkhn/ui/lib/Form/InputTypes";
 import {
   Button,
   CircularProgress,
   makeStyles,
   Typography,
+  TextField,
+  MenuItem,
 } from "@material-ui/core";
-import { TFunction } from "i18next";
+import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 
 import {
@@ -56,72 +56,6 @@ type CredentialFormInputs = {
   model: ModelEnum;
 };
 
-const credentialInputs: (
-  t: TFunction
-) => FormInputProperty<CredentialFormInputs>[] = (t) => [
-  {
-    type: "text",
-    name: "host",
-    label: t("host"),
-    variant: "outlined",
-    validationRules: { required: true },
-  },
-  {
-    type: "number",
-    name: "port",
-    label: t("port"),
-    variant: "outlined",
-    validationRules: { required: true },
-  },
-  {
-    type: "text",
-    name: "database",
-    label: t("database"),
-    variant: "outlined",
-    validationRules: { required: true },
-  },
-  {
-    type: "text",
-    name: "login",
-    label: t("username"),
-    variant: "outlined",
-    validationRules: { required: true },
-  },
-  {
-    type: "text",
-    password: true,
-    name: "password",
-    label: t("password"),
-    variant: "outlined",
-  },
-  {
-    type: "select",
-    selectOptions: [
-      {
-        id: "MSSQL",
-        label: "MSSQL",
-      },
-      {
-        id: "POSTGRES",
-        label: "POSTGRESQL",
-      },
-      {
-        id: "ORACLE",
-        label: "ORACLE",
-      },
-      {
-        id: "SQLLITE",
-        label: "SQLITE",
-      },
-    ],
-    name: "model",
-    label: t("vendor"),
-    variant: "outlined",
-    defaultValue: "POSTGRES",
-    validationRules: { required: true },
-  },
-];
-
 type CredentialFormProps = {
   source: Source;
   credential?: Credential;
@@ -133,6 +67,13 @@ const CredentialForm = ({
 }: CredentialFormProps): JSX.Element => {
   const { t } = useTranslation();
   const classes = useStyles();
+  const { register, reset, handleSubmit } = useForm<CredentialFormInputs>({
+    defaultValues: credential ? { ...credential } : {},
+  });
+
+  useEffect(() => {
+    reset(credential ? { ...credential } : {});
+  }, [credential]);
 
   const [
     createCredential,
@@ -160,30 +101,59 @@ const CredentialForm = ({
 
   return (
     <div className={classes.formContainer}>
-      <Form<CredentialFormInputs>
-        properties={credentialInputs(t)}
-        submit={handleCredentialSubmit}
-        formStyle={{ display: "block" }}
-        defaultValues={credential}
-        displaySubmitButton={false}
-        formFooter={
-          <Button
-            className={classes.button}
-            type="submit"
-            variant="contained"
-            color="primary"
-            fullWidth={false}
-          >
-            {isLoading ? (
-              <CircularProgress color="inherit" size={23} />
-            ) : (
-              <Typography>
-                {credential ? t("updateCredential") : t("createCredential")}
-              </Typography>
-            )}
-          </Button>
-        }
-      />
+      <form onSubmit={handleSubmit(handleCredentialSubmit)}>
+        <TextField required label={t("host")} inputRef={register("host").ref} />
+        <TextField required label={t("port")} inputRef={register("port").ref} />
+        <TextField
+          required
+          label={t("database")}
+          inputRef={register("database").ref}
+        />
+        <TextField
+          required
+          label={t("username")}
+          inputRef={register("login").ref}
+        />
+        <TextField
+          required
+          label={t("password")}
+          inputRef={register("password").ref}
+        />
+        <TextField
+          required
+          select
+          label={t("vendor")}
+          inputRef={register("model").ref}
+        >
+          <MenuItem key={"MSSQL"} value={"MSSQL"}>
+            MSSQL
+          </MenuItem>
+          <MenuItem key={"POSTGRES"} value={"POSGRES"}>
+            POSTGRESQL
+          </MenuItem>
+          <MenuItem key={"ORACLE"} value={"ORACLE"}>
+            ORACLE
+          </MenuItem>
+          <MenuItem key={"SQLLITE"} value={"SQLLITE"}>
+            SQLITE
+          </MenuItem>
+        </TextField>
+        <Button
+          className={classes.button}
+          type="submit"
+          variant="contained"
+          color="primary"
+          fullWidth={false}
+        >
+          {isLoading ? (
+            <CircularProgress color="inherit" size={23} />
+          ) : (
+            <Typography>
+              {credential ? t("updateCredential") : t("createCredential")}
+            </Typography>
+          )}
+        </Button>
+      </form>
     </div>
   );
 };
