4f89c55952ee39257fa87a470d7b58261d6f4317
BPierrick <pierrick.bellut95@gmail.com>
2021-04-12 13:27:56 +0200

    body scroll container

--- app/src/app/routes/CreateMapping.tsx
+++ app/src/app/routes/CreateMapping.tsx
@@ -1,28 +1,49 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import { Container, Button, makeStyles, Typography } from "@material-ui/core";
+import BackIcon from "@material-ui/icons/ArrowBackIos";
+import clsx from "clsx";
+import { useHistory } from "react-router-dom";
 
 import MappingCreationStepper from "features/mappingCreation/MappingCreationStepper";
 
+const FOOTER_HEIGHT = 150;
+
 const useStyles = makeStyles((theme) => ({
   footerContainer: {
-    position: "fixed",
-    bottom: 0,
-    height: 150,
+    height: FOOTER_HEIGHT,
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
     boxShadow: `0px 0px 10px ${theme.palette.divider}`,
     width: "100%",
   },
+  rootContainer: {
+    height: `calc(100vh - ${FOOTER_HEIGHT + theme.spacing(10)}px)`,
+  },
+  scrollContainer: {
+    overflowY: "auto",
+  },
   button: {
     margin: theme.spacing(2),
     textTransform: "none",
   },
+  previousButton: {
+    color: theme.palette.text.secondary,
+    "&:hover": {
+      backgroundColor: "inherit",
+      color: theme.palette.text.primary,
+    },
+  },
+  absolute: {
+    position: "absolute",
+  },
 }));
 
 const CreateMapping = (): JSX.Element => {
+  const stepperRef = useRef<HTMLDivElement>();
   const classes = useStyles();
+  const history = useHistory();
   const [activeStep, setActiveStep] = useState(0);
 
   const handlePrevStep = () => {
@@ -31,14 +52,39 @@ const CreateMapping = (): JSX.Element => {
   const handleNextStep = () => {
     activeStep < 3 && setActiveStep(activeStep + 1);
   };
+  const handleCancelClick = () => {
+    history.goBack();
+  };
   return (
     <>
-      <Container>
-        <MappingCreationStepper activeStep={activeStep} />
+      <Button
+        className={clsx(
+          classes.button,
+          classes.previousButton,
+          classes.absolute
+        )}
+        startIcon={<BackIcon />}
+        onClick={handleCancelClick}
+        disableRipple
+      >
+        <Typography>Cancel</Typography>
+      </Button>
+      <Container className={classes.rootContainer}>
+        <MappingCreationStepper ref={stepperRef} activeStep={activeStep} />
+        <div
+          className={classes.scrollContainer}
+          style={{
+            height: `calc(100% - ${stepperRef.current?.clientHeight}px)`,
+          }}
+        ></div>
       </Container>
       <div className={classes.footerContainer}>
-        <Button className={classes.button} onClick={handlePrevStep}>
-          <Typography color="textSecondary">Previous step</Typography>
+        <Button
+          className={clsx(classes.button, classes.previousButton)}
+          onClick={handlePrevStep}
+          disableRipple
+        >
+          <Typography>Previous step</Typography>
         </Button>
         <Button
           className={classes.button}
--- app/src/features/mappingCreation/MappingCreationStepper.tsx
+++ app/src/features/mappingCreation/MappingCreationStepper.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
 import { Stepper, Step, StepLabel, makeStyles } from "@material-ui/core";
 import { useTranslation } from "react-i18next";
@@ -16,39 +16,42 @@ type MappingCreationStepperProps = {
   activeStep?: number;
 };
 
-const MappingCreationStepper = ({
-  activeStep,
-}: MappingCreationStepperProps): JSX.Element => {
-  const classes = useStyles();
-  const { t } = useTranslation();
+const MappingCreationStepper = forwardRef(
+  ({ activeStep }: MappingCreationStepperProps, ref) => {
+    const classes = useStyles();
+    const { t } = useTranslation();
 
-  return (
-    <Stepper
-      className={classes.root}
-      alternativeLabel
-      activeStep={activeStep}
-      connector={<StepConnector />}
-    >
-      <Step>
-        <StepLabel StepIconComponent={StepIcon}>
-          {t("defineSourceTable")}
-        </StepLabel>
-      </Step>
-      <Step>
-        <StepLabel StepIconComponent={StepIcon}>
-          {t("selectFhirResource")}
-        </StepLabel>
-      </Step>
-      <Step>
-        <StepLabel StepIconComponent={StepIcon}>
-          {t("chooseFhirProfile")}
-        </StepLabel>
-      </Step>
-      <Step>
-        <StepLabel StepIconComponent={StepIcon}>{t("nameMapping")}</StepLabel>
-      </Step>
-    </Stepper>
-  );
-};
+    return (
+      <Stepper
+        className={classes.root}
+        alternativeLabel
+        activeStep={activeStep}
+        connector={<StepConnector />}
+        ref={ref}
+      >
+        <Step>
+          <StepLabel StepIconComponent={StepIcon}>
+            {t("defineSourceTable")}
+          </StepLabel>
+        </Step>
+        <Step>
+          <StepLabel StepIconComponent={StepIcon}>
+            {t("selectFhirResource")}
+          </StepLabel>
+        </Step>
+        <Step>
+          <StepLabel StepIconComponent={StepIcon}>
+            {t("chooseFhirProfile")}
+          </StepLabel>
+        </Step>
+        <Step>
+          <StepLabel StepIconComponent={StepIcon}>{t("nameMapping")}</StepLabel>
+        </Step>
+      </Stepper>
+    );
+  }
+);
+
+MappingCreationStepper.displayName = "MappingCreationStepper";
 
 export default MappingCreationStepper;
