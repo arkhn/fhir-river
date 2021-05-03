392cc18b60c80ba2d4095e9db360380681ee32a6
Valentin Matton <taq@protonmail.ch>
2021-05-03 12:29:45 +0200

    chore(prod): enable forwarded host by default

--- django/river/settings/prod.py
+++ django/river/settings/prod.py
@@ -3,3 +3,5 @@ from .base import *
 # TODO: Requests should be authenticated. The following is a temporary workaround that
 #   will be removed once authencation is setup globally.
 REST_FRAMEWORK["DEFAULT_PERMISSION_CLASSES"] = ["rest_framework.permissions.AllowAny"]
+
+USE_X_FORWARDED_HOST = os.environ.get("USE_X_FORWARDED_HOST", True)
