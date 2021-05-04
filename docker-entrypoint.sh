249f613aadbc7014c4aa57dd340a90f4e373d9e9
Valentin Matton <taq@protonmail.ch>
2021-05-04 16:01:12 +0200

    chore(uwsgi): keep http protocol by default

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -21,6 +21,15 @@ else
   else
     export UWSGI_PROCESSES=${UWSGI_PROCESSES:-5}
     export UWSGI_THREADS=${UWSGI_THREADS:-4}
+
+    # Keep using http protocol by default
+    # TODO(vmttn): drop http. This require updating the deployment to make sure
+    #   consuming services use the reverse proxy to communicate.
+    if [[ ! -z "${UWSGI_USE_SOCKET}" ]]; then
+      export UWSGI_SOCKET=0.0.0.0:8000
+    else
+      export UWSGI_HTTP=0.0.0.0:8000
+    fi
     python django/manage.py check --deploy
     uwsgi --ini uwsgi.ini
   fi
