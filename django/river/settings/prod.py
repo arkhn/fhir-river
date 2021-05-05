72cd4abab28ac2ca25b165fb6675985b77d40c86
Valentin Matton <taq@protonmail.ch>
2021-05-05 11:31:20 +0200

    chore: doc

--- django/river/settings/prod.py
+++ django/river/settings/prod.py
@@ -4,4 +4,7 @@ from .base import *
 #   will be removed once authencation is setup globally.
 REST_FRAMEWORK["DEFAULT_PERMISSION_CLASSES"] = ["rest_framework.permissions.AllowAny"]
 
+# https://docs.djangoproject.com/en/3.2/ref/settings/#use-x-forwarded-host
+# Behind a proxy, use the actual host as defined by the proxy. This is needed to
+# properly build urls.
 USE_X_FORWARDED_HOST = os.environ.get("USE_X_FORWARDED_HOST", True)
