8cae5cd8c5b81fc950ec527fa1eb17377f4ad56c
Jasopaum <jason.paumier@gmail.com>
2021-05-07 12:25:59 +0200

    [producer] Serialize bytes as string when generating kafka event

--- django/common/kafka/producer.py
+++ django/common/kafka/producer.py
@@ -15,6 +15,12 @@ class CustomJSONEncoder(json.JSONEncoder):
             return float(obj)
         if isinstance(obj, (datetime.date, datetime.datetime)):
             return obj.isoformat()
+        if isinstance(obj, bytes):
+            # Python doesn't know how to serialize bytes
+            # We arbitrarily choose utf-8. Note that if we need to decode bytes another
+            # way, we'll need to use a cleaning script that basically does
+            # val.encode("utf-8").decode("<your-encoding>")
+            return obj.decode("utf-8")
         return super().default(obj)
 
 
