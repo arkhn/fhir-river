9fca505f96afcf809aa17710812f49b89654940f
simonvadee <simon.vadee@gmail.com>
2021-05-12 14:15:46 +0200

    feat(app): template index.html using env variables

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -9,4 +9,9 @@ set -e
 export STATIC_ROOT="${STATIC_ROOT:-/var/www/static/pyrog-app}"
 
 yarn build
+
+# template build/index.html using environment variables
+sed -i -e "s/{{REACT_APP_API_URL}}/$REACT_APP_API_URL/g" \
+    ./build/index.html
+
 rm -rf "${STATIC_ROOT}" && cp -r ./build "${STATIC_ROOT}"
--- app/src/services/api/urls.ts
+++ app/src/services/api/urls.ts
@@ -1,1 +1,8 @@
-export const { REACT_APP_API_URL: API_URL } = process.env;
+let API_URL = "" as string | undefined;
+if (process.env.NODE_ENV === "production") {
+  ({ REACT_APP_API_URL: API_URL } = (window as any).env);
+} else {
+  ({ REACT_APP_API_URL: API_URL } = process.env);
+}
+
+export { API_URL };
