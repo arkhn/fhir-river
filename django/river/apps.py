47c3c6900512c00dd2f3f5ee83643ddc15719b05
Valentin Matton <taq@protonmail.ch>
2021-05-17 15:36:11 +0200

    structure: cleaning
    chore: remove asgi module
    chore: refacto river package out
    chore(settings): helper to parse array value
    chore(settings): helper to read bool value
    chore: remove dev settings file
    chore(settings): remove tests settings module
    fix(compose): remove dup
    chore(compose): same pg creds everywhere
    chore(docker): use base settings
    chore: allow to connect to kafka from host
    chore(tests): add kafka mark
    chore(tests): add fixture to reset kafka topics
    feat: add river app

---
+++ django/river/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RiverConfig(AppConfig):
+    name = "river"
--- django/river/settings/test.py
+++
@@ -1,5 +0,0 @@
-from .prod import *
-
-SECRET_KEY = "USE_IN_TEST_ONLY"
-
-REST_FRAMEWORK["DEFAULT_PERMISSION_CLASSES"] = ["rest_framework.permissions.AllowAny"]
