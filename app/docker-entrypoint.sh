01d7b469c0168396ece8eccadb04203cf4c94f5e
simonvadee <simon.vadee@gmail.com>
2021-05-18 11:05:10 +0200

    build(app): fix sed delimiter in entrypoint

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -10,11 +10,11 @@ export STATIC_ROOT="${STATIC_ROOT:-/var/www/static/pyrog-app}"
 
 # template build/index.html using environment variables
 sed -i \
-    -e "s/{{PUBLIC_URL}}/$PUBLIC_URL/g" \
-    -e "s/{{API_URL}}/$API_URL/g" \
-    -e "s/{{OIDC_LOGIN_URL}}/$OIDC_LOGIN_URL/g" \
-    -e "s/{{OIDC_LOGOUT_URL}}/$OIDC_LOGOUT_URL/g" \
-    -e "s/{{CSRF_COOKIE_NAME}}/$CSRF_COOKIE_NAME/g" \
+    -e "s|{{PUBLIC_URL}}|$PUBLIC_URL|g" \
+    -e "s|{{API_URL}}|$API_URL|g" \
+    -e "s|{{OIDC_LOGIN_URL}}|$OIDC_LOGIN_URL|g" \
+    -e "s|{{OIDC_LOGOUT_URL}}|$OIDC_LOGOUT_URL|g" \
+    -e "s|{{CSRF_COOKIE_NAME}}|$CSRF_COOKIE_NAME|g" \
     ./build/index.html
 
 rm -rf "${STATIC_ROOT}" && cp -r ./build "${STATIC_ROOT}"
