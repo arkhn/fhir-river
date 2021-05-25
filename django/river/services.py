e548734e27957a28f517ba7ccb93b0d0bb55b6c0
Valentin Matton <taq@protonmail.ch>
2021-05-25 12:24:06 +0200

    service: refactor service
    feat(tests): add batch service layer
    feat(river): add batch service layer
    feat(api): use batch service layer

---
+++ django/river/services.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from typing import List
+
+from river import models
+from river.adapters.event_publisher import EventPublisher
+from river.adapters.mappings import MappingsRepository
+from river.adapters.topics import TopicsHandler
+from river.domain.events import BatchResource
+
+
+def batch(
+    resources: List[str], topics: TopicsHandler, publisher: EventPublisher, mappings: MappingsRepository
+) -> models.Batch:
+    batch_instance = models.Batch.objects.create()
+
+    for base_topic in ["batch", "extract", "transform", "load"]:
+        topics.create(f"{base_topic}.{batch_instance.id}")
+
+    for resource_id in resources:
+        # Ensure the mapping exists
+        mappings.get(resource_id)
+
+        publisher.publish(
+            topic=f"batch.{batch_instance.id}",
+            event=BatchResource(batch_id=batch_instance.id, resource_id=resource_id),
+        )
+
+    return batch_instance
+
+
+def abort(batch: models.Batch, topics: TopicsHandler) -> None:
+    for base_topic in ["batch", "extract", "transform", "load"]:
+        topics.delete(f"{base_topic}.{batch.id}")
+
+    batch.deleted_at = datetime.now()
+    batch.save(update_fields=["deleted_at"])
+
+
+def retry(batch: models.Batch) -> None:
+    pass
---
+++ tests/river/unit/test_services.py
@@ -0,0 +1,38 @@
+import uuid
+
+import pytest
+
+from river import models, services
+from river.adapters.event_publisher import FakeEventPublisher
+from river.adapters.topics import FakeTopics
+from river.domain.events import BatchResource
+
+pytestmark = pytest.mark.django_db
+
+
+def test_batch():
+    topics = FakeTopics()
+    publisher = FakeEventPublisher()
+    resources = [str(uuid.uuid4()) for _ in range(5)]
+
+    batch_instance = services.batch(resources, topics, publisher)
+
+    assert batch_instance is not None
+    assert models.Batch.objects.filter(id=batch_instance.id).exists()
+    assert topics._topics == {
+        f"{base_topic}.{batch_instance.id}" for base_topic in ["batch", "extract", "transform", "load"]
+    }
+    assert publisher._events[f"batch.{batch_instance.id}"] == {
+        BatchResource(batch_id=batch_instance.id, resource_id=r) for r in resources
+    }
+
+
+def test_abort(batch):
+    topics = FakeTopics(
+        topics=[f"{base_topic}.{batch.id}" for base_topic in ["batch", "extract", "transform", "load"]]
+    )
+
+    services.abort(batch, topics)
+
+    assert batch.deleted_at is not None
+    assert topics._topics == set()
