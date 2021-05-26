2e844cb595fe90d39da8e13696a334c79a87e1a2
Valentin Matton <taq@protonmail.ch>
2021-05-26 15:14:40 +0200

    feat(tests): use fake mapping repo in units

--- tests/river/unit/test_services.py
+++ tests/river/unit/test_services.py
@@ -4,6 +4,7 @@ import pytest
 
 from river import models, services
 from river.adapters.event_publisher import FakeEventPublisher
+from river.adapters.mappings import FakeMappingsRepository
 from river.adapters.topics import FakeTopics
 from river.domain.events import BatchResource
 
@@ -14,8 +15,9 @@ def test_batch():
     topics = FakeTopics()
     publisher = FakeEventPublisher()
     resources = [str(uuid.uuid4()) for _ in range(5)]
+    mappings_repo = FakeMappingsRepository(mappings={id: {} for id in resources})
 
-    batch_instance = services.batch(resources, topics, publisher)
+    batch_instance = services.batch(resources, topics, publisher, mappings_repo)
 
     assert batch_instance is not None
     assert models.Batch.objects.filter(id=batch_instance.id).exists()
@@ -25,6 +27,7 @@ def test_batch():
     assert publisher._events[f"batch.{batch_instance.id}"] == {
         BatchResource(batch_id=batch_instance.id, resource_id=r) for r in resources
     }
+    assert mappings_repo._seen == resources
 
 
 def test_abort(batch):
