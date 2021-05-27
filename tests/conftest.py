fd72b2391458a900f6c5c5a12f88d54c2308eaa8
Valentin Matton <taq@protonmail.ch>
2021-05-27 20:58:43 +0200

    fix(tests): missing fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,13 @@
+import json
 import time
+from pathlib import Path
 
 import pytest
 from confluent_kafka import admin
 
 from django.conf import settings
+
+DATA_FIXTURES_DIR = Path(__file__).resolve().parent / "fixtures"
 
 
 @pytest.fixture
@@ -52,3 +56,9 @@ def clear_topics(request):
 
     if request.node.get_closest_marker("kafka"):
         request.addfinalizer(_clear_topics)
+
+
+@pytest.fixture
+def patient_mapping():
+    with (DATA_FIXTURES_DIR / "patient_mapping.json").open() as f:
+        return json.load(f)
