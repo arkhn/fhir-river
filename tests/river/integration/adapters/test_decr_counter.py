98059d2c5751863c69c4b4ea813de42c3a389205
Valentin Matton <taq@protonmail.ch>
2021-05-27 17:15:57 +0200

    feat(tests): add decrementing counter

---
+++ tests/river/integration/adapters/test_decr_counter.py
@@ -0,0 +1,10 @@
+from river.adapters.decr_counter import RedisDecrementingCounter
+
+
+def test_can_decr_counter():
+    counter = RedisDecrementingCounter()
+
+    counter.set("foo", 10)
+    assert counter.get("foo") == 10
+    counter.decr("foo")
+    assert counter.get("foo") == 9
