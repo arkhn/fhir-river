b4d526be663a0abe54aa41155e2e48781fc1c610
Valentin Matton <taq@protonmail.ch>
2021-05-27 17:45:10 +0200

    feat(river): use sub adapter and multiple topics

--- django/common/service/event.py
+++
@@ -1,7 +0,0 @@
-import uuid
-
-
-class Event:
-    def __init__(self, data: dict) -> None:
-        self.id = uuid.uuid4()
-        self.data = data
--- django/common/service/service.py
+++ django/common/service/service.py
@@ -1,33 +1,41 @@
 import logging
+import re
+from typing import Callable, Dict, Type
 
-from common.kafka.consumer import Consumer
-from common.service.event import Event
-from common.service.handler import Handler
 from confluent_kafka.error import ConsumeError
 
+from river.adapters.event_subscriber import EventSubscriber
+from river.domain import events
+
+logger = logging.getLogger(__name__)
 event_logger = logging.getLogger("river.event")
 
 
 class Service:
     """Stateful service"""
 
-    def __init__(self, consumer: Consumer, handler: Handler) -> None:
-        self.consumer = consumer
-        self.handler = handler
+    def __init__(self, subscriber: EventSubscriber, handlers: Dict[str, Callable[[Type[events.Event]], None]]) -> None:
+        self.subscriber = subscriber
+        self.handlers = handlers
 
     def run(self):
-        with self.consumer.subscribe() as subscription:
+        topics = list(self.handlers.keys())
+        with self.subscriber.subscribe(topics):
             while True:
                 try:
-                    data = subscription()
+                    topic, raw = self.subscriber.poll()
                 except ConsumeError as err:
-                    logging.error(err)
+                    logger.error(err)
                     raise err
 
-                event = Event(data)
+                try:
+                    # topic can be regexp
+                    topic = next(t for t in self.handlers.keys() if re.match(t, topic))
+                except StopIteration:
+                    logger.error(f"Unhandled topic: {topic}")
 
                 try:
-                    self.handler(event)
+                    self.handlers[topic](raw)
                 except Exception:
                     event_logger.exception("Failed to process event")
                     continue
