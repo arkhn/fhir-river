00e47e7b637712692332d12b09cdada34e5d0fcf
Valentin Matton <taq@protonmail.ch>
2021-05-27 20:39:55 +0200

    feat(events): integrate extractor events

--- django/river/domain/events.py
+++ django/river/domain/events.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Any, Optional
 
 
 class Event:
@@ -9,3 +10,12 @@ class Event:
 class BatchResource(Event):
     batch_id: str
     resource_id: str
+    primary_key_values: Optional[Any] = None
+
+
+@dataclass(frozen=True)
+class ExtractedRecord(Event):
+    batch_id: str
+    resource_type: str
+    resource_id: str
+    record: Any
