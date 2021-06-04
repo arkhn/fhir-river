54a96788ae3653d6b036144e0b7628a23b827169
Teva RIOU <teva.riou@gmail.com>
2021-06-04 15:39:54 +0200

    feat(app): add batch page container

--- app/src/features/Batches/Batches.tsx
+++ app/src/features/Batches/Batches.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import { Container } from "@material-ui/core";
+
 const Batches = (): JSX.Element => {
-  return <></>;
+  return (
+    <Container maxWidth="xl">
+      <div>ok</div>
+    </Container>
+  );
 };
 
 export default Batches;
