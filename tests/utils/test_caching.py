4a216ce5295d7ff80e2d524f9f3071044bfd11a8
Valentin Matton <taq@protonmail.ch>
2021-06-11 15:01:25 +0200

    chore(tests): add cache expiration

--- tests/utils/test_caching.py
+++ tests/utils/test_caching.py
@@ -1,11 +1,17 @@
+from datetime import timedelta
+from time import sleep
+from uuid import uuid4
+
+import pytest
+
 from utils.caching import cache
 
 
-def test_can_cache():
+def test_caching_with_decorator():
     execution_counter = 0
 
     @cache("first_param", backend="memory")
-    def func(first_param: int):
+    def func(first_param: int) -> int:
         nonlocal execution_counter
         execution_counter += 1
         return first_param * 2
@@ -15,4 +21,27 @@ def test_can_cache():
     assert func(2) == 4
     assert execution_counter == 1
     assert func(3) == 6
+    assert execution_counter == 2
+
+
+@pytest.mark.redis
+def test_cache_can_expire():
+    execution_counter = 0
+
+    random_input = str(uuid4())
+
+    @cache("first_param", backend="redis", expire_after=timedelta(milliseconds=50))
+    def func(first_param: str) -> str:
+        nonlocal execution_counter
+        execution_counter += 1
+        return first_param * 2
+
+    assert func(random_input) == random_input * 2
+    assert execution_counter == 1
+    assert func(random_input) == random_input * 2
+    assert execution_counter == 1
+
+    sleep(0.1)
+
+    assert func(random_input) == random_input * 2
     assert execution_counter == 2
