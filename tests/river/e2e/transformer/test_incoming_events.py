a670ef5e598f3ac5c47e4066ec8e289418196ebc
Valentin Matton <taq@protonmail.ch>
2021-06-15 12:03:04 +0200

    chore(tests): outline transformer e2e test

--- tests/river/e2e/transformer/test_incoming_events.py
+++ tests/river/e2e/transformer/test_incoming_events.py
@@ -2,7 +2,7 @@ import pytest
 
 from river.adapters.event_publisher import KafkaEventPublisher
 from river.adapters.event_subscriber import KafkaEventSubscriber
-from river.domain.events import BatchResource
+from river.domain.events import ExtractedRecord
 
 pytestmark = [pytest.mark.django_db, pytest.mark.kafka]
 
@@ -12,12 +12,19 @@ def test_resource_batch(batch):
     kafka_publisher = KafkaEventPublisher()
     kafka_subscriber = KafkaEventSubscriber(group_id="foo")
 
-    kafka_publisher.publish(f"batch.{batch.id}", BatchResource(batch_id=batch.id, resource_id="foo"))
+    kafka_publisher.publish(
+        f"extract.{batch.id}",
+        ExtractedRecord(
+            batch_id=batch.id,
+            resource_type="foo",
+            resource_id="foo",
+            record={"users_user_email_b77906f9": "didier@chloroquine.org"},
+        ),
+    )
 
     # TODO:
-    # - start extractor service in a thread
+    # - start transformer service in a thread
     # - use mapping
-    # - add SQLite inmemory DB for extraction
 
-    with kafka_subscriber.subscribe([f"extract.{batch.id}"]):
+    with kafka_subscriber.subscribe([f"transform.{batch.id}"]):
         kafka_subscriber.poll()
