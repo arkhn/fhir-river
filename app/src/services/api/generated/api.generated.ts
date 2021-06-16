ab9eec1660dc4f6d1eab32e26b91bad0ca853b1f
Teva RIOU <teva.riou@gmail.com>
2021-06-16 15:19:37 +0200

    feat(app): update api schema

--- app/src/services/api/generated/api.generated.ts
+++ app/src/services/api/generated/api.generated.ts
@@ -66,7 +66,10 @@ export const api = createApi({
       ApiBatchesListApiResponse,
       ApiBatchesListApiArg
     >({
-      query: () => ({ url: `/api/batches/` }),
+      query: (queryArg) => ({
+        url: `/api/batches/`,
+        params: { limit: queryArg.limit, offset: queryArg.offset },
+      }),
     }),
     apiBatchesCreate: build.mutation<
       ApiBatchesCreateApiResponse,
@@ -733,8 +736,13 @@ export type ApiAttributesDestroyApiArg = {
   /** A unique value identifying this attribute. */
   id: string;
 };
-export type ApiBatchesListApiResponse = /** status 200  */ Batch[];
-export type ApiBatchesListApiArg = {};
+export type ApiBatchesListApiResponse = /** status 200  */ PaginatedBatchList;
+export type ApiBatchesListApiArg = {
+  /** Number of results to return per page. */
+  limit?: number;
+  /** The initial index from which to return the results. */
+  offset?: number;
+};
 export type ApiBatchesCreateApiResponse = /** status 201  */ Batch;
 export type ApiBatchesCreateApiArg = {
   batchRequest: BatchRequest;
@@ -1129,6 +1137,12 @@ export type Batch = {
   resources: {
     [key: string]: any;
   };
+};
+export type PaginatedBatchList = {
+  count?: number;
+  next?: string | null;
+  previous?: string | null;
+  results?: Batch[];
 };
 export type BatchRequest = {
   resources: {
