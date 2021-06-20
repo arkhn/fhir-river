814a857ad3588b6bb7dd1e86fd7f02449574f46a
Valentin Matton <taq@protonmail.ch>
2021-06-20 19:58:36 +0200

    chore(tests): fix fixtures name

--- tests/river/unit/test_parsing.py
+++ tests/river/unit/test_parsing.py
@@ -3,13 +3,13 @@ from river.parsing import Source, as_old_mapping
 
 
 def test_mappings_from_exported_json(load_mapping_fixture):
-    source = load_mapping_fixture("mimic_export_new_format.json")
+    source = load_mapping_fixture("mimic_new_mapping_format.json")
 
     assert Source(**source)
 
 
 def test_as_old_mapping(load_mapping_fixture):
-    source_data = load_mapping_fixture("mimic_export_new_format.json")
+    source_data = load_mapping_fixture("mimic_new_mapping_format.json")
     source = Source(**source_data)
 
     old_mapping_data = as_old_mapping(source, source.resources[0].id)
