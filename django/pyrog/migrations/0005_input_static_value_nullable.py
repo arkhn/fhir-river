e3887d657576f43098a11f917c1d5e1bfd4c5dad
Teva RIOU <teva.riou@gmail.com>
2021-06-23 15:08:37 +0200

    fix(api): alter input static value empty string to null

--- django/pyrog/migrations/0005_input_static_value_nullable.py
+++ django/pyrog/migrations/0005_input_static_value_nullable.py
@@ -3,6 +3,15 @@
 from django.db import migrations, models
 
 
+def update_input_static_value(apps, schema_editor):
+    Input = apps.get_model("pyrog", "Input")
+
+    for input_ in Input.objects.all():
+        if input_.static_value == "":
+            input_.static_value = None
+            input_.save(update_fields=["static_value"])
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -15,4 +24,5 @@ class Migration(migrations.Migration):
             name="static_value",
             field=models.TextField(blank=True, default=None, null=True),
         ),
+        migrations.RunPython(update_input_static_value, reverse_code=migrations.RunPython.noop),
     ]
