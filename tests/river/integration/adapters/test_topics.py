b62daf1c58885ab54f091b0bcfd512196ac37cec
simonvadee <simon.vadee@gmail.com>
2021-06-24 16:08:35 +0200

    fix(tests): kafka topic already exist in unit tests

--- tests/river/integration/adapters/test_topics.py
+++ tests/river/integration/adapters/test_topics.py
@@ -9,19 +9,19 @@ pytestmark = pytest.mark.kafka
 def test_topics_can_create_and_delete_topic():
     topics = KafkaTopics()
 
-    topics.create("foo")
+    topics.create("bar")
 
     # Asserts that the topic has been created by trying to recreate it
     # That way the internal implementation does not leak into our tests
     with pytest.raises(Exception):
-        topics.create("foo")
+        topics.create("bar")
 
     # Deletion can fail if creation is not yet done
     for attempt in tenacity.Retrying(
         stop=tenacity.stop_after_delay(3), wait=tenacity.wait.wait_fixed(1), reraise=True
     ):
         with attempt:
-            topics.delete("foo")
+            topics.delete("bar")
 
     # Asserts that the topic has been deleted by trying to redelete it
     with pytest.raises(Exception):
@@ -33,4 +33,4 @@ def test_topics_can_create_and_delete_topic():
             reraise=True,
         ):
             with attempt:
-                topics.delete("foo")
+                topics.delete("bar")
