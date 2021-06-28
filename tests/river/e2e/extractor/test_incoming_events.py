96a760b4e84cf3d3079b9a95ba3174fe12302a23
Teva RIOU <teva.riou@gmail.com>
2021-06-28 12:19:13 +0200

    test: test extractor incoming events

--- tests/river/e2e/extractor/test_incoming_events.py
+++ tests/river/e2e/extractor/test_incoming_events.py
@@ -2,7 +2,7 @@ import pytest
 
 from river.adapters.event_publisher import KafkaEventPublisher
 from river.adapters.event_subscriber import KafkaEventSubscriber
-from river.domain.events import BatchResource
+from river.domain.events import BatchEvent
 
 pytestmark = [pytest.mark.django_db, pytest.mark.kafka]
 
@@ -12,7 +12,7 @@ def test_resource_batch(batch):
     kafka_publisher = KafkaEventPublisher()
     kafka_subscriber = KafkaEventSubscriber(group_id="foo")
 
-    kafka_publisher.publish(f"batch.{batch.id}", BatchResource(batch_id=batch.id, resource_id="foo"))
+    kafka_publisher.publish(f"batch.{batch.id}", BatchEvent(batch_id=batch.id, resource_id="foo"))
 
     with kafka_subscriber.subscribe([f"extract.{batch.id}"]):
         kafka_subscriber.poll()
