0fb323ab009926e98872f8760e309cbd8079b37a
Jasopaum <jason.paumier@gmail.com>
2021-06-28 18:48:56 +0200

    [tests] Fix test for ProgressionCounter

--- tests/river/integration/adapters/test_progression_counter.py
+++ tests/river/integration/adapters/test_progression_counter.py
@@ -8,11 +8,11 @@ def test_can_decr_counter():
     counter = RedisProgressionCounter()
 
     counter.set_extracted("foo", 10)
-    assert counter.get("foo") == 10, None
+    assert counter.get("foo") == (10, None)
 
     counter.increment_loaded("foo")
-    assert counter.get("foo") == 10, 1
+    assert counter.get("foo") == (10, 1)
 
     for _ in range(5):
         counter.increment_loaded("foo")
-    assert counter.get("foo") == 10, 6
+    assert counter.get("foo") == (10, 6)
