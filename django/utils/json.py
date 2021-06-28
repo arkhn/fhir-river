7894c654d2bd17be11bf9f62a5311fc883ca8472
Jasopaum <jason.paumier@gmail.com>
2021-06-28 18:47:10 +0200

    [json-encoder] Use chardet to detect encoding

--- django/utils/json.py
+++ django/utils/json.py
@@ -1,6 +1,11 @@
 import datetime
 import decimal
 import json
+import logging
+
+import chardet
+
+logger = logging.getLogger("utils.CustomJSONEncoder")
 
 
 class CustomJSONEncoder(json.JSONEncoder):
@@ -12,9 +17,12 @@ class CustomJSONEncoder(json.JSONEncoder):
             return obj.isoformat()
         if isinstance(obj, bytes):
             # FIXME: this should be done differently, issue #376
-            # Python doesn't know how to serialize bytes
-            # We arbitrarily choose utf-8. Note that if we need to decode bytes another
+            # Python doesn't know how to serialize bytes so we use chardet to find the
+            # right encoding.
+            # Note that if we need to decode bytes another
             # way, we'll need to use a cleaning script that basically does
-            # val.encode("utf-8").decode("<your-encoding>")
-            return obj.decode("utf-8")
+            # val.encode("<detected-encoding>").decode("<your-encoding>")
+            detected_encoding = chardet.detect(obj)["encoding"]
+            logger.debug(f"Detected encoding {detected_encoding} for {obj}")
+            return obj.decode(detected_encoding)
         return super().default(obj)
