6051e0f004e7f046ad145f5f57d253f081403dcc
Teva RIOU <teva.riou@gmail.com>
2021-06-29 14:01:04 +0200

    fix(river): useless argument in event_subscriber.py

--- django/river/adapters/event_subscriber.py
+++ django/river/adapters/event_subscriber.py
@@ -71,7 +71,7 @@ class KafkaEventSubscriber(EventSubscriber):
                 logger.debug(error.str())
                 self._handle_error(error)
                 continue
-            return msg.topic(), json.loads(msg.value(None))
+            return msg.topic(), json.loads(msg.value())
 
     def _handle_error(self, error: KafkaError):
         """Handles a KafkaError"""
