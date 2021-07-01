c7f958de08054e8efd9a6c77d1cad33c9d5ca348
Jasopaum <jason.paumier@gmail.com>
2021-07-01 17:29:03 +0200

    [doc] Add docstring to adapters/progression_counter.py

--- django/river/adapters/progression_counter.py
+++ django/river/adapters/progression_counter.py
@@ -6,6 +6,23 @@ from django.conf import settings
 
 
 class ProgressionCounter:
+    """Abstract class used to track the progression of an ETL.
+
+    A `ProgressionCounter` is basically a key-value store.
+    The keys should identify a (batch, pyrog source) couple and each key gives access to
+    2 values. One of this value is the number of resources extracted for the
+    corresponding key and the other is the number of resources currenlty loaded in the
+    target DB.
+
+    A `ProgressionCounter` should have 3 methods:
+        - set_extracted: sets the value corresponding to the number of extracted
+            resources
+        - increment_loaded: increments by 1 the value corresponding to the number of
+            loaded resources
+        - get: returns both counters for a specified key
+
+    """
+
     def set_extracted(self, id: str, value: int):
         raise NotImplementedError
 
@@ -17,13 +34,28 @@ class ProgressionCounter:
 
 
 class FakeProgressionCounter(ProgressionCounter):
+    """ProgressionCounter using an in-memory dict as a data structure.
+
+    Attributes:
+        _count (dict of str: dict): The dict in which are stored the counters.
+        It has the form:
+            {
+                "key1": {"extracted": nb_extracted_1, "loaded": nb_loaded_1},
+                "key2": {"extracted": nb_extracted_2, "loaded": nb_loaded_2},
+                ...
+            }
+
+    """
+
     def __init__(self, counts=dict()):
         self._count = counts
 
-    def set_extracted(self, id: str, value: int):
+    def set_extracted(self, id: str, value: int) -> None:
+        """Sets the value corresponding to the number of extracted resources."""
         self._count[id] = {"extracted": value}
 
-    def increment_loaded(self, id: str):
+    def increment_loaded(self, id: str) -> None:
+        """Increments by 1 the value corresponding to the number of loaded resources."""
         if id not in self._count:
             self._count[id] = {}
         if "loaded" not in self._count[id]:
@@ -31,10 +63,27 @@ class FakeProgressionCounter(ProgressionCounter):
         self._count[id]["loaded"] += 1
 
     def get(self, id: str) -> Tuple[Optional[int], Optional[int]]:
+        """Gets both counters for the specified key.
+
+        Args:
+            id: The key corresponding to the current (batch, resource).
+
+        Returns:
+            A tuple with the number of extracted resources (or None if not found) and
+                the number of loaded resources (or None if not found).
+
+        """
         return self._count.get(id, {}).get("extracted"), self._count.get(id, {}).get("loaded")
 
 
 class RedisProgressionCounter(ProgressionCounter):
+    """ProgressionCounter using an in-memory dict as a data structure.
+
+    Attributes:
+        _client (redis.Redis): The redis client used to access the backend redis DB.
+
+    """
+
     def __init__(self):
         self._client = redis.Redis(
             host=settings.REDIS_HOST,
@@ -42,13 +91,25 @@ class RedisProgressionCounter(ProgressionCounter):
             db=settings.REDIS_DB,
         )
 
-    def set_extracted(self, id: str, value: int):
+    def set_extracted(self, id: str, value: int) -> None:
+        """Sets the value corresponding to the number of extracted resources."""
         self._client.hset("extracted_counters", id, value)
 
-    def increment_loaded(self, id: str):
+    def increment_loaded(self, id: str) -> None:
+        """Increments by 1 the value corresponding to the number of loaded resources."""
         self._client.hincrby("loaded_counters", id, 1)
 
     def get(self, id: str) -> Tuple[Optional[int], Optional[int]]:
+        """Gets both counters for the specified key.
+
+        Args:
+            id: The key corresponding to the current (batch, resource).
+
+        Returns:
+            A tuple with the number of extracted resources (or None if not found)
+                and the number of loaded resources (or None if not found).
+
+        """
         raw_extracted = self._client.hget("extracted_counters", id)
         raw_loaded = self._client.hget("loaded_counters", id)
         extracted = int(raw_extracted) if raw_extracted else None
