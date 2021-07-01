48442caee65f744d0adf1b285d8a2fbf00f342c7
Jasopaum <jason.paumier@gmail.com>
2021-07-01 16:53:00 +0200

    [doc] Add docstring to adapters/mappings.py

--- django/river/adapters/mappings.py
+++ django/river/adapters/mappings.py
@@ -18,33 +18,56 @@ def build_repository_key(batch_id: str, resource_id: str):
 
 
 class MappingsRepository:
-    """This class is an abstraction that is used when we need to fetch a mapping.
-    It has a single method `get` with the mapping id as argument.
+    """This class is used when we need to fetch a mapping.
+
+    An MappingsRepository is basically an abstraction over a key-value store. It should
+    have a method `set` and a method `get`.
     """
 
     def set(self, batch_id: str, resource_id: str, mapping: Any):
+        """Sets a mapping at a specified key."""
         raise NotImplementedError
 
     def get(self, batch_id: str, resource_id: str):
+        """Gets the mapping at a specified key."""
         raise NotImplementedError
 
 
 class FakeMappingsRepository(MappingsRepository):
+    """MappingsRepository that uses an in-memory dict as a key-value store.
+
+    Attributes:
+        _mappings (dict of str: dict): dict used as a key-value store.
+        _seen (list): used in tests to check which mappings have been fetched from this
+            repository.
+
+    """
+
     def __init__(self, mappings: dict = None):
         self._mappings = mappings or {}
         self._seen = []
 
     def set(self, batch_id: str, resource_id: str, mapping: Any):
+        """Sets the mapping at the key built from batch_id and resource_id."""
         key = build_repository_key(batch_id, resource_id)
         self._mappings[key] = mapping
 
     def get(self, batch_id: str, resource_id: str):
+        """Gets the mapping at the key built from batch_id and resource_id."""
         key = build_repository_key(batch_id, resource_id)
         self._seen.append(key)
         return self._mappings[key]
 
 
 class RedisMappingsRepository(MappingsRepository):
+    """MappingsRepository that uses redis as a backend.
+
+    Attributes:
+        mapping_redis (redis.Redis): a redis client used to communicate with the
+            redis DB.
+
+    """
+
     def __init__(self):
         self.mapping_redis = redis.Redis(
             host=settings.REDIS_HOST,
@@ -53,10 +76,12 @@ class RedisMappingsRepository(MappingsRepository):
         )
 
     def set(self, batch_id: str, resource_id: str, mapping: Any):
+        """Sets the mapping at the key built from batch_id and resource_id."""
         key = build_repository_key(batch_id, resource_id)
         self.mapping_redis.set(key, mapping)
 
     def get(self, batch_id: str, resource_id: str):
+        """Gets the mapping at the key built from batch_id and resource_id."""
         key = build_repository_key(batch_id, resource_id)
         serialized_mapping = self.mapping_redis.get(key)
         if serialized_mapping is None:
