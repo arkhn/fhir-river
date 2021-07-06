f16e8dc2f30d08a7924be9296d599b798e49e008
BPierrick <pierrick.bellut95@gmail.com>
2021-07-06 16:17:39 +0200

    Add source export

--- app/src/app/routes/Sources/SourceMappings.tsx
+++ app/src/app/routes/Sources/SourceMappings.tsx
@@ -1,20 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import { Icon } from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
 import {
   Button,
+  CircularProgress,
   Container,
   Grid,
   makeStyles,
   Typography,
 } from "@material-ui/core";
+import { useParams } from "react-router";
 
 import MappingsTable from "features/Mappings/MappingsTable";
 import MappingsToolbar from "features/Mappings/MappingsToolbar";
 import NavigationBreadcrumbs from "features/NavigationBreadcrumbs/NavigationBreadcrumbs";
 import CredentialEditButton from "features/Sources/CredentialEditButton";
 import SourceDrawer from "features/Sources/SourceDrawer";
+import { useApiSourcesExportRetrieveQuery } from "services/api/generated/api.generated";
 
 const useStyles = makeStyles((theme) => ({
   header: {
@@ -35,6 +38,31 @@ const useStyles = makeStyles((theme) => ({
 
 const SourceMappings = (): JSX.Element => {
   const classes = useStyles();
+  const { sourceId } = useParams<{ sourceId?: string }>();
+  const exportButtonRef = useRef<HTMLAnchorElement | null>(null);
+  const {
+    data: serializedSource,
+    isLoading: isSerializedSourceLoading,
+  } = useApiSourcesExportRetrieveQuery(
+    { id: sourceId ?? "" },
+    { skip: !sourceId }
+  );
+
+  const handleExportMappingClick = () => {
+    if (serializedSource) {
+      const data = `text/json;charset=utf-8,${encodeURIComponent(
+        JSON.stringify(serializedSource)
+      )}`;
+
+      if (exportButtonRef && exportButtonRef.current) {
+        exportButtonRef.current.setAttribute("href", `data:${data}`);
+        exportButtonRef.current.setAttribute(
+          "download",
+          `${serializedSource.name}.json`
+        );
+      }
+    }
+  };
 
   return (
     <>
@@ -51,11 +79,16 @@ const SourceMappings = (): JSX.Element => {
             <Button
               size="small"
               variant="contained"
+              ref={exportButtonRef}
+              component="a"
               className={classes.button}
+              disabled={isSerializedSourceLoading}
               startIcon={
                 <Icon icon={IconNames.EXPORT} className={classes.icon} />
               }
+              onClick={handleExportMappingClick}
             >
+              {isSerializedSourceLoading && <CircularProgress size="small" />}
               <Typography>Export mapping</Typography>
             </Button>
             <Button
