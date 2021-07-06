2c8741059359cbf2da1bb7d2e14c8b6f664a2348
Teva RIOU <teva.riou@gmail.com>
2021-07-06 11:22:23 +0200

    style(app): menu paper max height

--- app/src/features/Batches/BatchCreate.tsx
+++ app/src/features/Batches/BatchCreate.tsx
@@ -22,7 +22,6 @@ import {
 } from "services/api/endpoints";
 
 const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -55,7 +54,7 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: theme.typography.fontWeightRegular,
   },
   menuPaper: {
-    maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+    maxHeight: ITEM_HEIGHT * 4.5 + theme.spacing(1),
     width: 250,
   },
 }));
