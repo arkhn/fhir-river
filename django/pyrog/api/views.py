88151096eced1257cc06f87f033c3320ecac65b6
Teva RIOU <teva.riou@gmail.com>
2021-07-12 17:54:37 +0200

    feat(api): order objects by creation date

--- django/pyrog/api/views.py
+++ django/pyrog/api/views.py
@@ -1,3 +1,4 @@
+from rest_framework import filters as drf_filters
 from rest_framework import viewsets
 from rest_framework.decorators import action
 
@@ -28,6 +29,9 @@ class FhirProxyView(ProxyView):
 class SourceViewSet(viewsets.ModelViewSet):
     queryset = models.Source.objects.all()
     serializer_class = basic_serializers.SourceSerializer
+    filter_backends = [drf_filters.OrderingFilter]
+    ordering_fields = ["created_at"]
+    ordering = ["created_at"]
 
     @action(detail=False, methods=["post"], serializer_class=MappingSerializer, url_path="import")
     def import_mapping(self, request):
@@ -55,50 +59,64 @@ class SourceViewSet(viewsets.ModelViewSet):
 class ResourceViewSet(viewsets.ModelViewSet):
     queryset = models.Resource.objects.all()
     serializer_class = basic_serializers.ResourceSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
+    filter_backends = [django_filters.DjangoFilterBackend, drf_filters.OrderingFilter]
     filterset_class = filters.ResourceFilterSet
+    ordering_fields = ["created_at"]
+    ordering = ["created_at"]
 
 
 class CredentialViewSet(viewsets.ModelViewSet):
     queryset = models.Credential.objects.all()
     serializer_class = basic_serializers.CredentialSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
+    filter_backends = [django_filters.DjangoFilterBackend, drf_filters.OrderingFilter]
     filterset_class = filters.CredentialFilterSet
+    ordering_fields = ["created_at"]
+    ordering = ["created_at"]
 
 
 class AttributeViewSet(viewsets.ModelViewSet):
     queryset = models.Attribute.objects.all()
     serializer_class = basic_serializers.AttributeSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
+    filter_backends = [django_filters.DjangoFilterBackend, drf_filters.OrderingFilter]
     filterset_class = filters.AttributeFilterSet
+    ordering_fields = ["created_at"]
+    ordering = ["created_at"]
 
 
 class InputGroupViewSet(viewsets.ModelViewSet):
     queryset = models.InputGroup.objects.all()
     serializer_class = basic_serializers.InputGroupSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
+    filter_backends = [django_filters.DjangoFilterBackend, drf_filters.OrderingFilter]
     filterset_class = filters.InputGroupFilterSet
+    ordering_fields = ["created_at"]
+    ordering = ["created_at"]
 
 
 class InputViewSet(viewsets.ModelViewSet):
     queryset = models.Input.objects.all()
     serializer_class = basic_serializers.InputSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
+    filter_backends = [django_filters.DjangoFilterBackend, drf_filters.OrderingFilter]
     filterset_class = filters.InputFilterSet
+    ordering_fields = ["created_at"]
+    ordering = ["created_at"]
 
 
 class ColumnViewSet(viewsets.ModelViewSet):
     queryset = models.Column.objects.all()
     serializer_class = basic_serializers.ColumnSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
+    filter_backends = [django_filters.DjangoFilterBackend, drf_filters.OrderingFilter]
     filterset_class = filters.ColumnFilterSet
+    ordering_fields = ["created_at"]
+    ordering = ["created_at"]
 
 
 class JoinViewSet(viewsets.ModelViewSet):
     queryset = models.Join.objects.all()
     serializer_class = basic_serializers.JoinSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
+    filter_backends = [django_filters.DjangoFilterBackend, drf_filters.OrderingFilter]
     filterset_class = filters.JoinFilterSet
+    ordering_fields = ["created_at"]
+    ordering = ["created_at"]
 
 
 class ConditionViewSet(viewsets.ModelViewSet):
