00d88a4e11656cde8dbb0b9661fa21a8fde44910
Teva RIOU <teva.riou@gmail.com>
2021-07-13 18:58:22 +0200

    tests(extractor): handler

--- tests/river/unit/extractor/test_handler.py
+++ tests/river/unit/extractor/test_handler.py
@@ -1,7 +1,6 @@
 import pytest
 
 from river.adapters.event_publisher import FakeEventPublisher
-from river.adapters.mappings import FakeMappingsRepository
 from river.adapters.progression_counter import FakeProgressionCounter, Progression
 from river.common.analyzer import Analyzer
 from river.domain.events import BatchEvent, ExtractedRecord
@@ -15,10 +14,9 @@ def test_batch_resource_handler(batch, users_to_patients_mapping):
     event = BatchEvent(batch_id=batch.id, resource_id=resource_id)
     publisher = FakeEventPublisher()
     counter = FakeProgressionCounter()
-    mappings_repo = FakeMappingsRepository({f"{batch.id}:{resource_id}": users_to_patients_mapping})
     analyzer = Analyzer()
 
-    batch_resource_handler(event, publisher, counter, analyzer, mappings_repo)
+    batch_resource_handler(event, publisher, counter, analyzer)
 
     assert f"extract.{batch.id}" in publisher._events
     assert len(publisher._events[f"extract.{batch.id}"]) > 0
