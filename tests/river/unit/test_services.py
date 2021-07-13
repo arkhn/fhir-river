e68b9097cd5014d124446f94e39b05764d89297a
Teva RIOU <teva.riou@gmail.com>
2021-07-13 18:43:49 +0200

    tests(river): adapt batch and preview

--- tests/river/unit/test_services.py
+++ tests/river/unit/test_services.py
@@ -2,10 +2,7 @@ import uuid
 
 import pytest
 
-from river import models
 from river.adapters.event_publisher import FakeEventPublisher
-from river.adapters.mappings import FakeMappingsRepository
-from river.adapters.pyrog_client import FakePyrogClient
 from river.adapters.topics import FakeTopicsManager
 from river.domain.events import BatchEvent
 from river.services import abort, batch, preview
@@ -16,22 +13,15 @@ pytestmark = pytest.mark.django_db
 def test_batch():
     topics = FakeTopicsManager()
     publisher = FakeEventPublisher()
-    resources = [str(uuid.uuid4()) for _ in range(5)]
-    pyrog_client = FakePyrogClient(mappings={id: {} for id in resources})
-    mappings_repo = FakeMappingsRepository()
+    batch_id = str(uuid.uuid4())
+    resources = [{"id": str(uuid.uuid4())} for _ in range(5)]
 
-    batch_instance = batch(resources, topics, publisher, pyrog_client, mappings_repo)
+    batch(batch_id, resources, topics, publisher)
 
-    assert batch_instance is not None
-    assert models.Batch.objects.filter(id=batch_instance.id).exists()
-    assert topics._topics == {
-        f"{base_topic}.{batch_instance.id}" for base_topic in ["batch", "extract", "transform", "load"]
-    }
-    assert publisher._events[f"batch.{batch_instance.id}"] == [
-        BatchEvent(batch_id=batch_instance.id, resource_id=r) for r in resources
+    assert topics._topics == {f"{base_topic}.{batch_id}" for base_topic in ["batch", "extract", "transform", "load"]}
+    assert publisher._events[f"batch.{batch_id}"] == [
+        BatchEvent(batch_id=batch_id, resource_id=resource["id"]) for resource in resources
     ]
-    assert pyrog_client._seen == resources
-    assert mappings_repo._mappings == {f"{batch_instance.id}:{resource_id}": "{}" for resource_id in resources}
 
 
 def test_abort(batch):
@@ -52,9 +42,7 @@ def test_retry(batch):
 
 # TODO improve test to verify what's in documents
 def test_preview(users_to_patients_mapping):
-    pyrog_client = FakePyrogClient(mappings={"foo": users_to_patients_mapping})
+    mapping = {"resources": [{"id": "foo"}]}
+    documents, errors = preview(mapping, None)
 
-    documents, errors = preview("foo", None, pyrog_client=pyrog_client)
-
-    assert pyrog_client._seen == ["foo"]
     assert len(errors) == 0
