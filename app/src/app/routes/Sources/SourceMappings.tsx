472c0835ab8dbeb0c1732ae061b71bd7e6d06ea1
BPierrick <pierrick.bellut95@gmail.com>
2021-07-13 11:56:07 +0200

    Add source export

--- app/src/app/routes/Sources/SourceMappings.tsx
+++ app/src/app/routes/Sources/SourceMappings.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import { Icon } from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
-import { Container, Grid, makeStyles } from "@material-ui/core";
+import {
+  Container,
+  Grid,
+  makeStyles,
+  CircularProgress,
+  Typography,
+} from "@material-ui/core";
 import { useTranslation } from "react-i18next";
 import { useHistory, useParams } from "react-router";
 
@@ -12,6 +18,7 @@ import MappingsToolbar from "features/Mappings/MappingsToolbar";
 import NavigationBreadcrumbs from "features/NavigationBreadcrumbs/NavigationBreadcrumbs";
 import CredentialEditButton from "features/Sources/CredentialEditButton";
 import SourceDrawer from "features/Sources/SourceDrawer";
+import { useApiSourcesExportRetrieveQuery } from "services/api/generated/api.generated";
 
 const useStyles = makeStyles((theme) => ({
   header: {
@@ -31,50 +38,80 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const SourceMappings = (): JSX.Element => {
-  const classes = useStyles();
   const { t } = useTranslation();
+  const classes = useStyles();
   const history = useHistory();
   const { sourceId } = useParams<{ sourceId?: string }>();
 
+  const exportButtonRef = useRef<HTMLAnchorElement | null>(null);
+  const {
+    data: serializedSource,
+    isLoading: isSerializedSourceLoading,
+  } = useApiSourcesExportRetrieveQuery(
+    { id: sourceId ?? "" },
+    { skip: !sourceId }
+  );
+
+  const handleExportMappingClick = () => {
+    if (serializedSource) {
+      const data = `text/json;charset=utf-8,${encodeURIComponent(
+        JSON.stringify(serializedSource)
+      )}`;
+
+      if (exportButtonRef && exportButtonRef.current) {
+        exportButtonRef.current.setAttribute("href", `data:${data}`);
+        exportButtonRef.current.setAttribute(
+          "download",
+          `${serializedSource.name}.json`
+        );
+      }
+    }
+  };
   const handleLaunchClick = () => {
     history.push(`/sources/${sourceId}/batches`);
   };
 
   return (
     <>
-      <div className={classes.header}>
-        <NavigationBreadcrumbs />
-        <Grid>
-          <CredentialEditButton
-            variant="contained"
-            className={classes.button}
-            color="secondary"
-            startIcon={<Icon icon={IconNames.COG} />}
-          >
-            {t("databaseSettings")}
-          </CredentialEditButton>
-          <Button
-            variant="contained"
-            color="secondary"
-            className={classes.button}
-            startIcon={<Icon icon={IconNames.EXPORT} />}
-          >
-            {t("exportMapping")}
-          </Button>
-          <Button
-            variant="contained"
-            color="secondary"
-            className={classes.button}
-            startIcon={<Icon icon={IconNames.FLAME} />}
-            onClick={handleLaunchClick}
-          >
-            {t("launchEtl")}
-          </Button>
-        </Grid>
-      </div>
-      <Container maxWidth="xl" className={classes.container}>
-        <MappingsToolbar />
-        <MappingsTable />
+      <Container maxWidth="xl">
+        <div className={classes.header}>
+          <NavigationBreadcrumbs />
+          <Grid>
+            <CredentialEditButton
+              variant="contained"
+              className={classes.button}
+              color="secondary"
+              startIcon={<Icon icon={IconNames.COG} />}
+            />
+            <Button
+              component="a"
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              startIcon={<Icon icon={IconNames.EXPORT} />}
+              ref={exportButtonRef}
+              disabled={isSerializedSourceLoading}
+              onClick={handleExportMappingClick}
+              disableElevation
+            >
+              {isSerializedSourceLoading && <CircularProgress size="small" />}
+              <Typography>{t("exportMapping")}</Typography>
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              startIcon={<Icon icon={IconNames.FLAME} />}
+              onClick={handleLaunchClick}
+            >
+              <Typography>{t("launchETL")}</Typography>
+            </Button>
+          </Grid>
+        </div>
+        <Container maxWidth="xl" className={classes.container}>
+          <MappingsToolbar />
+          <MappingsTable />
+        </Container>
       </Container>
       <SourceDrawer />
     </>
