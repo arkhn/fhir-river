193944cc4643969164b2687ef198a8901d5f1980
nriss <nicolas.riss22@gmail.com>
2021-07-16 16:20:29 +0200

    catch(error): merge_instant arguments > 2 or = 0

--- tests/common/scripts/merging/test_merge_instant.py
+++ tests/common/scripts/merging/test_merge_instant.py
@@ -3,3 +3,8 @@ from common.scripts.merging import merge_instant
 
 def test_merge_instant():
     assert merge_instant("2015-02-07", "13:28:17") == "2015-02-07T13:28:17+02:00"
+
+    try:
+        merge_instant("11111", "2222", "3333")
+    except ValueError:
+        assert True
