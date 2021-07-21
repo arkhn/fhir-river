b35a21149ccb946034810602ffbaed0749a1749a
nriss <nicolas.riss22@gmail.com>
2021-07-21 10:44:23 +0200

    fix(assert tests): remove useless assert in test_merge_instant

--- tests/common/scripts/merging/test_merge_instant.py
+++ tests/common/scripts/merging/test_merge_instant.py
@@ -5,12 +5,8 @@ import pytest
 def test_merge_instant():
     assert merge_instant("2015-02-07", "13:28:17") == "2015-02-07T13:28:17+02:00"
 
-    with pytest.raises(ValueError) as exc_info:
+    with pytest.raises(ValueError):
         merge_instant("11111", "2222", "3333")
 
-    assert exc_info.type is ValueError
-
-    with pytest.raises(ValueError) as exc_info:
+    with pytest.raises(ValueError):
         merge_instant()
-
-    assert exc_info.type is ValueError
