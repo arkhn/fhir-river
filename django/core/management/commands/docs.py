63fe7fcb438f473891db9662ddc059b52858b43e
simonvadee <simon.vadee@gmail.com>
2021-07-22 14:21:34 +0200

    docs(river): document how to spin up the docs webserver

--- django/core/management/commands/docs.py
+++ django/core/management/commands/docs.py
@@ -5,10 +5,10 @@ from pdoc import cli as pdoc_cli
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument("--port", nargs=1, default="8080")
+        parser.add_argument("--http", default="0.0.0.0:8080", metavar="HOST:PORT")
 
     def handle(self, *args, **options):
         args = pdoc_cli.parser.parse_args()
         args.modules = ["common", "pagai", "pyrog", "river", "users", "utils"]
-        args.http = f"0.0.0.0:{options.get('port')}"
+        args.http = options.get("http")
         pdoc_cli.main(_args=args)
