2e10ce6b1f5beb5e419735d97a51da14a64770de
Teva RIOU <teva.riou@gmail.com>
2021-08-02 15:55:30 +0200

    chore(river): clean fetch mapping

--- tests/river/unit/common/test_fetch_mapping.py
+++ tests/river/unit/common/test_fetch_mapping.py
@@ -3,23 +3,23 @@ from pathlib import Path
 from pytest import raises
 from rest_framework.exceptions import NotAuthenticated, PermissionDenied
 
-from river.common.mapping import fetch_mapping
+from river.common.mapping import fetch_concept_maps
 
 FIXTURES_DIR = Path(__file__).resolve().parent / "fixtures"
 
 
 def test_fetch_resource_mapping_invalid(mock_pyrog_response, mock_fhir_api_response):
     with raises(NotAuthenticated):
-        fetch_mapping.fetch_concept_map("id", "Bearer invalidToken")
+        fetch_concept_maps.fetch_concept_map("id", "Bearer invalidToken")
 
 
 def test_fetch_resource_mapping_forbidden(mock_pyrog_response, mock_fhir_api_response):
     with raises(PermissionDenied):
-        fetch_mapping.fetch_concept_map("id", "Bearer forbiddenToken")
+        fetch_concept_maps.fetch_concept_map("id", "Bearer forbiddenToken")
 
 
 def test_fetch_concept_map(mock_fhir_api_response):
-    actual = fetch_mapping.fetch_concept_map("id", "Bearer validToken")
+    actual = fetch_concept_maps.fetch_concept_map("id", "Bearer validToken")
     expected = {
         "F": "female",
         "M": "male",
@@ -29,17 +29,17 @@ def test_fetch_concept_map(mock_fhir_api_response):
 
 def test_fetch_concept_map_invalid(mock_fhir_api_response):
     with raises(NotAuthenticated):
-        fetch_mapping.fetch_concept_map("id", "Bearer invalidToken")
+        fetch_concept_maps.fetch_concept_map("id", "Bearer invalidToken")
 
 
 def test_fetch_concept_map_forbidden(mock_fhir_api_response):
     with raises(PermissionDenied):
-        fetch_mapping.fetch_concept_map("id", "Bearer forbiddenToken")
+        fetch_concept_maps.fetch_concept_map("id", "Bearer forbiddenToken")
 
 
 def test_dereference_concept_map(mock_fhir_api_response):
     mapping = {"attributes": [{"inputGroups": [{"inputs": [{"conceptMapId": "cm_gender"}]}]}]}
 
-    fetch_mapping.dereference_concept_map(mapping, "Bearer validToken")
+    fetch_concept_maps.dereference_concept_map(mapping, "Bearer validToken")
 
     assert mapping["attributes"][0]["inputGroups"][0]["inputs"][0]["conceptMap"] == {"F": "female", "M": "male"}
