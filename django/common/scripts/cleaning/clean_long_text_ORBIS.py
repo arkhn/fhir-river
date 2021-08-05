eaf980a143dc8e89b082aa6e2070e86b8fb8f721
Peltier10 <sophie.peltier@mailo.com>
2021-08-05 17:08:39 +0200

    remove clean_text_blob and its test, add readability on clean_long_text_ORBIS

--- django/common/scripts/cleaning/clean_long_text_ORBIS.py
+++ django/common/scripts/cleaning/clean_long_text_ORBIS.py
@@ -29,27 +29,22 @@ def clean_long_text_ORBIS(raw_input):
         y = re.findall("\\\f:[\\d]{4}\\\\?", text)
         text = text.replace(x[0], "").replace(y[0], "")
 
-
-    if text[0].isalpha():
-        if not text[1].isalpha():
-            x = re.findall("^(.{2})", text)
-            text = text.replace(x[0], "")
-
-    if not text[0].isalpha():
-        if text[1].isalpha():
-            x = re.findall("^(.{2})", text)
-            text = text.replace(x[0], "")
-
-    if "\f:12" in text:
+    elif "\f:12" in text:
         x = re.findall("(.*)(?=\\f:[\\d]{5})", text)
         y = re.findall("\\\f:[\\d]{5}\\\\?", text)
         text = text.replace(x[0], "").replace(y[0], "")
 
-    if not text[0].isalpha():
-        if not text[1].isalpha():
-            x = re.findall("^(.{2})", text)
-            text = text.replace(x[0], "")
-
+    else :
+        if not text[0].isalpha():
+            if not text[1].isalpha():
+                x = re.findall("^(.{2})", text)
+                text = text.replace(x[0], "")
+            else : 
+                x = re.findall("^(.{2})", text)
+                text = text.replace(x[0], "")
+        else :
+            if not text[1].isalpha():
+                x = re.findall("^(.{2})", text)
+                text = text.replace(x[0], "")
 
     return text
-
