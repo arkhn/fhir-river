e6bffd55e9cf638b8e4decdcb857659b6414df49
Teva RIOU <teva.riou@gmail.com>
2021-08-07 18:50:08 +0200

    test(app): concept maps

--- app/src/common/hooks/useMergeConceptMapsToMappings.ts
+++ app/src/common/hooks/useMergeConceptMapsToMappings.ts
@@ -1,3 +1,9 @@
+import {
+  getConceptMapIdsFromMappings,
+  getConceptMappingFromResource,
+  mergeConceptMapsToMappings,
+  CustomConceptMapById,
+} from "common/helpers/conceptMaps";
 import { useApiConceptMapsListQuery } from "services/api/endpoints";
 import type { Mapping } from "services/api/generated/api.generated";
 
@@ -6,69 +12,22 @@ const useMergeConceptMapsToMappings = (params: {
 }): Mapping | undefined => {
   const { mappings } = params;
 
-  const ids =
-    mappings?.resources?.reduce(
-      (acc, resource) =>
-        resource.attributes
-          ? [
-              ...acc,
-              ...resource.attributes.reduce(
-                (acc, attribute) =>
-                  attribute.input_groups
-                    ? [
-                        ...acc,
-                        ...attribute.input_groups.reduce(
-                          (acc, input_group) =>
-                            input_group.inputs
-                              ? [
-                                  ...acc,
-                                  ...input_group.inputs.reduce(
-                                    (acc, input) =>
-                                      input.concept_map_id
-                                        ? [...acc, input.concept_map_id]
-                                        : acc,
-                                    [] as string[]
-                                  ),
-                                ]
-                              : acc,
-                          [] as string[]
-                        ),
-                      ]
-                    : acc,
-                [] as string[]
-              ),
-            ]
-          : acc,
-      [] as string[]
-    ) ?? [];
+  const conceptMapIds = mappings && getConceptMapIdsFromMappings(mappings);
 
-  // NOTE we only handle a single target for each source
   const { conceptMapsById } = useApiConceptMapsListQuery(
-    { ids: ids },
+    { ids: conceptMapIds },
     {
-      skip: !ids,
+      skip: !conceptMapIds,
       selectFromResult: ({ data: conceptMaps }) => ({
         conceptMapsById: conceptMaps?.reduce(
           (acc, conceptMap) =>
             conceptMap.id
               ? {
-                  [conceptMap.id]: conceptMap.group?.reduce(
-                    (acc, group) => ({
-                      ...group.element.reduce(
-                        (acc, el) =>
-                          el.code
-                            ? { [el.code]: el.target?.[0]?.code, ...acc }
-                            : acc,
-                        {} as Record<string, string | undefined>
-                      ),
-                      ...acc,
-                    }),
-                    {} as Record<string, string | undefined>
-                  ),
+                  [conceptMap.id]: getConceptMappingFromResource(conceptMap),
                   ...acc,
                 }
               : acc,
-          {} as Record<string, Record<string, string | undefined> | undefined>
+          {} as CustomConceptMapById
         ),
       }),
     }
@@ -76,26 +35,8 @@ const useMergeConceptMapsToMappings = (params: {
 
   return (
     mappings &&
-    conceptMapsById && {
-      ...mappings,
-      resources: mappings.resources?.map((resource) => ({
-        ...resource,
-        attributes: resource.attributes?.map((attribute) => ({
-          ...attribute,
-          input_groups: attribute.input_groups?.map((input_group) => ({
-            ...input_group,
-            inputs: input_group.inputs?.map((input) =>
-              input.concept_map_id && conceptMapsById
-                ? {
-                    ...input,
-                    concept_map: conceptMapsById[input.concept_map_id],
-                  }
-                : { ...input }
-            ),
-          })),
-        })),
-      })),
-    }
+    conceptMapsById &&
+    mergeConceptMapsToMappings(mappings, conceptMapsById)
   );
 };
 
