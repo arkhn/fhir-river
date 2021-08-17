15b85066448dbec34a5922494adf039628ac351a
Teva RIOU <teva.riou@gmail.com>
2021-08-17 16:13:17 +0200

    fix(explore): rewrite base path from pagai to api

--- django/pagai/urls.py
+++ django/pagai/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from pagai.api import views
 
 urlpatterns = [
-    path("pagai/list-owners/", views.OwnersListView.as_view(), name="list-owners"),
-    path("pagai/owner-schema/<owner>/", views.OwnerSchemaView.as_view(), name="owner-schema"),
-    path("pagai/explore/", views.ExploreView.as_view(), name="explore"),
+    path("api/list-owners/", views.OwnersListView.as_view(), name="list-owners"),
+    path("api/owner-schema/<owner>/", views.OwnerSchemaView.as_view(), name="owner-schema"),
+    path("api/explore/", views.ExploreView.as_view(), name="explore"),
 ]
