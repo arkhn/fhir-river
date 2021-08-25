8ca83886699f791d0d6afda0fe4baaeeb751af65
Valentin Matton <taq@protonmail.ch>
2021-08-25 16:05:56 +0200

    chore: more docs

--- django/authz.py
+++ django/authz.py
@@ -19,20 +19,24 @@ class Operation(enum.Enum):
     DELETE = "DELETE"
 
 
-HTTP_METHOD_TO_OPERATION = {
-    "GET": Operation.READ,
-    "POST": Operation.CREATE,
-    "PUT": Operation.UPDATE,
-    "DELETE": Operation.DELETE,
-}
-
-
 class AuthzBackend:
+    """Interface for a service that validates authz."""
+
     def validate(self, user_email: str, resource: str, operation: Operation) -> bool:
         raise NotImplementedError
 
 
 class RemoteAuthzBackend(AuthzBackend):
+    """
+    Concrete implementation which uses on a remote authz service.
+
+    The backend can be configured through the settings:
+
+    * ``settings.AUTHZ_MIDDLEWARE_ENDPOINT``: url to the authz validation endpoint.
+    * ``settings.AUTHZ_MIDDLEWARE_SERVICE_NAME``: the service name as registered to the
+        authz server.
+    """
+
     def __init__(self):
         if settings.AUTHZ_MIDDLEWARE_ENABLED and settings.AUTHZ_MIDDLEWARE_ENDPOINT is None:
             raise ImproperlyConfigured("AUTHZ_MIDDLEWARE_ENDPOINT is not set.")
@@ -57,6 +61,27 @@ class RemoteAuthzBackend(AuthzBackend):
 
 
 class AuthzMiddleware:
+    """
+    Middleware to enforce authz to incoming requests.
+
+    It relies on a authz backend to do the validation.
+    Both the middleware can be configured through the settings:
+
+    * ``settings.AUTHZ_MIDDLEWARE_ENABLED``: toggle the authz validation altogether
+    * ``settings.AUTHZ_MIDDLEWARE_BACKEND``: the backend class to use
+    * ``settings.AUTHZ_MIDDLEWARE_URL_PATTERN``: limit the authz validation to urls
+        matching the given regex pattern
+
+    Backends can be further configured.
+    """
+
+    HTTP_METHOD_TO_OPERATION = {
+        "GET": Operation.READ,
+        "POST": Operation.CREATE,
+        "PUT": Operation.UPDATE,
+        "DELETE": Operation.DELETE,
+    }
+
     def __init__(self, get_response, backend: AuthzBackend = None, url_pattern: str = None):
         self.get_response = get_response
         self._backend = backend or import_string(settings.AUTHZ_MIDDLEWARE_BACKEND)()
@@ -77,12 +102,19 @@ class AuthzMiddleware:
         return request.user.email
 
     def get_operation(self, request: http.HttpRequest) -> Operation:
-        return HTTP_METHOD_TO_OPERATION[request.method]
+        """
+        Find out which operation is associated to the request.
+
+        Currently uses a simple HTTP method mapping.
+        """
+        return self.HTTP_METHOD_TO_OPERATION[request.method]
 
     def get_resource(self, request: http.HttpRequest) -> str:
         return request.get_full_path()
 
     def is_exempt(self, request) -> bool:
+        """Return whether authz validation should be skipped."""
+
         return (
             not settings.AUTHZ_MIDDLEWARE_ENABLED
             or not request.user.is_authenticated
