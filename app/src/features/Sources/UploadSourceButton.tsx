d7cfa1e367fa7e77f188128dddaaaf991fbda89f
BPierrick <pierrick.bellut95@gmail.com>
2021-08-30 17:28:55 +0200

    feature: Add CredentialDialog to UploadSourceButton

--- app/src/features/Sources/UploadSourceButton.tsx
+++ app/src/features/Sources/UploadSourceButton.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { ChangeEvent, useRef, useState } from "react";
 
 import { Icon } from "@blueprintjs/core";
@@ -7,13 +6,13 @@ import { makeStyles } from "@material-ui/core";
 import { FetchBaseQueryError } from "@reduxjs/toolkit/dist/query";
 import { useTranslation } from "react-i18next";
 
-import { useAppDispatch } from "app/store";
 import Alert from "common/components/Alert";
 import Button from "common/components/Button";
 import { useApiSourcesImportCreateMutation } from "services/api/endpoints";
 import { apiValidationErrorFromResponse } from "services/api/errors";
+import { MappingRequest } from "services/api/generated/api.generated";
 
-import { sourceEdited } from "./sourceSlice";
+import CredentialDialog from "./CredentialDialog";
 
 const useStyles = makeStyles((theme) => ({
   fileInput: {
@@ -23,60 +22,32 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: theme.spacing(3),
     textTransform: "none",
     width: "auto",
-    minWidth: 150,
     "& svg": {
       fill: theme.palette.text.primary,
     },
   },
 }));
 
+type CredentialFormInputs = Omit<MappingRequest["credential"], "owners">;
+
 const UploadSourceButton = (): JSX.Element => {
   const { t } = useTranslation();
   const classes = useStyles();
-  const dispatch = useAppDispatch();
   const fileInputRef = useRef<HTMLInputElement | null>(null);
-  const [alert, setAlert] = useState<string | undefined>(undefined);
+  const [alert, setAlert] = useState<string | undefined>();
+  const [mappingRequest, setMappingRequest] = useState<
+    MappingRequest | undefined
+  >();
   const [apiSourceImportCreate] = useApiSourcesImportCreateMutation();
 
   let fileReader: FileReader | null = null;
 
   const handleAlertClose = () => setAlert(undefined);
-  const handleFileRead = async () => {
+  const handleFileRead = () => {
     const content = fileReader?.result;
     if (content) {
-      const parsedContent = JSON.parse(content as string);
-      try {
-        const {
-          created_at,
-          id,
-          name,
-          updated_at,
-        } = await apiSourceImportCreate({
-          mappingRequest: parsedContent,
-        }).unwrap();
-        dispatch(
-          sourceEdited({
-            created_at,
-            id,
-            name,
-            updated_at,
-            users: [],
-          })
-        );
-      } catch (error) {
-        // TODO: Handle errors nicely
-        console.error(error);
-        const errorData = apiValidationErrorFromResponse(
-          error as FetchBaseQueryError
-        );
-        if (errorData) {
-          const errorMessage = Object.entries(errorData).reduce(
-            (acc, [key, value]) => `${acc} ${key}: ${value}`,
-            ""
-          );
-          setAlert(errorMessage);
-        }
-      }
+      const parsedContent = JSON.parse(content as string) as MappingRequest;
+      setMappingRequest(parsedContent);
 
       //Clear input value
       if (fileInputRef.current) {
@@ -92,33 +63,71 @@ const UploadSourceButton = (): JSX.Element => {
       fileReader.readAsText(file);
     }
   };
+  const handleDialogClose = () => {
+    setMappingRequest(undefined);
+  };
+  const handleCredentialSubmit = async (
+    credentialInputs: CredentialFormInputs
+  ) => {
+    if (mappingRequest) {
+      try {
+        await apiSourceImportCreate({
+          mappingRequest: {
+            ...mappingRequest,
+            credential: { ...credentialInputs, ...mappingRequest.credential },
+          },
+        }).unwrap();
+        setMappingRequest(undefined);
+      } catch (error) {
+        // TODO: Handle errors nicely
+        console.error(error);
+        const errorData = apiValidationErrorFromResponse(
+          error as FetchBaseQueryError
+        );
+        if (errorData) {
+          const errorMessage = Object.entries(errorData).reduce(
+            (acc, [key, value]) => `${acc} ${key}: ${value}`,
+            ""
+          );
+          setAlert(errorMessage);
+        }
+      }
+    }
+  };
 
   return (
-    <label>
-      <input
-        className={classes.fileInput}
-        ref={fileInputRef}
-        // disabled={isLoading}
-        type="file"
-        accept=".json"
-        onChange={handleFileChange}
+    <>
+      <label>
+        <input
+          className={classes.fileInput}
+          ref={fileInputRef}
+          type="file"
+          accept=".json"
+          onChange={handleFileChange}
+        />
+        <Button
+          component="div"
+          className={classes.button}
+          variant="outlined"
+          fullWidth={false}
+          startIcon={<Icon icon={IconNames.IMPORT} />}
+        >
+          {t("importSource")}
+        </Button>
+      </label>
+      <CredentialDialog
+        open={!!mappingRequest}
+        onSubmit={handleCredentialSubmit}
+        onClose={handleDialogClose}
+        credential={mappingRequest?.credential}
       />
-      <Button
-        component="div"
-        className={classes.button}
-        variant="outlined"
-        fullWidth={false}
-        startIcon={<Icon icon={IconNames.IMPORT} />}
-      >
-        {t("importSource")}
-      </Button>
       <Alert
         severity="error"
         open={!!alert}
         onClose={handleAlertClose}
         message={alert}
       />
-    </label>
+    </>
   );
 };
 
