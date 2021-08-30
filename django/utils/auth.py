f9ebb4cc42c415de8605a13acd1b7905195418b6
simonvadee <simon.vadee@gmail.com>
2021-08-30 14:22:07 +0200

    docs(auth): add link to OIDC_OP_LOGOUT_URL_METHOD docs

--- django/utils/auth.py
+++ django/utils/auth.py
@@ -6,5 +6,6 @@ from django.conf import settings
 # will simply silently re-login. We therefore call the logout route of oprovider.
 # See https://mozilla-django-oidc.readthedocs.io/en/stable/installation.html
 # #log-user-out-of-the-openid-connect-provider
+# https://mozilla-django-oidc.readthedocs.io/en/stable/settings.html#OIDC_OP_LOGOUT_URL_METHOD
 def logout(request):
     return settings.OIDC_OP_LOGOUT_ENDPOINT
