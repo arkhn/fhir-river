08bc67ca9e0903944c073e502ef884b97a0500c0
Teva RIOU <teva.riou@gmail.com>
2021-08-31 12:26:28 +0200

    docs(river): batch source filter

--- django/river/api/filters.py
+++ django/river/api/filters.py
@@ -4,6 +4,8 @@ from river import models
 
 
 class BatchFilterSet(filters.FilterSet):
+    """BatchFilterSet filters a list of batches by sources of their resources"""
+
     source = filters.ModelMultipleChoiceFilter("resources__source", queryset=pyrog_models.Source.objects.all())
 
     class Meta:
