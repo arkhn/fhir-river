78986cfef0cea1bae0f85487485d586c81e6514b
Teva RIOU <teva.riou@gmail.com>
2021-09-01 15:36:24 +0200

    test(river): test batch fix

--- tests/river/unit/test_services.py
+++ tests/river/unit/test_services.py
@@ -9,16 +9,16 @@ pytestmark = pytest.mark.django_db
 
 
 def test_batch(batch_factory, resource_factory):
-    r1, r2 = resource_factory.create_batch(2)
-    b = batch_factory.create(resources=(r1, r2))
+    resources = resource_factory.create_batch(2)
+    batch_ = batch_factory.create(resources=resources)
     topics = FakeTopicsManager()
     publisher = FakeEventPublisher()
 
-    batch(b.id, [r1, r2], topics, publisher)
+    batch(batch_.id, resources, topics, publisher)
 
-    assert topics._topics == {f"{base_topic}.{b.id}" for base_topic in ["batch", "extract", "transform", "load"]}
-    assert publisher._events[f"batch.{b.id}"] == [
-        BatchEvent(batch_id=b.id, resource_id=resource_id) for resource_id in [r1.id, r2.id]
+    assert topics._topics == {f"{base_topic}.{batch_.id}" for base_topic in ["batch", "extract", "transform", "load"]}
+    assert publisher._events[f"batch.{batch_.id}"] == [
+        BatchEvent(batch_id=batch_.id, resource_id=resource.id) for resource in resources
     ]
 
 
