e9285368bfe71d5358a294694b9c97de84bc67dc
simonvadee <simon.vadee@gmail.com>
2021-09-01 18:34:01 +0200

    fix(tests): fix tests/pagai

---
+++ tests/pagai/integration/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+
+from sqlalchemy import create_engine
+
+from ..conftest import get_sql_url, load_table
+from ..settings import DATABASES
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_databases():
+    for db_driver in DATABASES:
+        db_config = {**DATABASES[db_driver], "model": db_driver}
+        sql_engine = create_engine(get_sql_url(db_driver, db_config))
+
+        # Load (or reload) test data into db.
+        load_table(sql_engine, "patients", "patients.csv")
+        load_table(sql_engine, "UPPERCASE", "patients-uppercase.csv")
+        load_table(sql_engine, "CaseSensitive", "patients-case-sensitive.csv")
