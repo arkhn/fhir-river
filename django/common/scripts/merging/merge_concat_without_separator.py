a4761af6b8f5568803edcc8580612f006d5e0a16
nriss <nicolas.riss22@gmail.com>
2021-09-01 16:41:26 +0200

    fix(merge_script): take jason review and remove clean_string

--- django/common/scripts/merging/merge_concat_without_separator.py
+++ django/common/scripts/merging/merge_concat_without_separator.py
@@ -2,4 +2,5 @@ def merge_concat_without_separator(*args):
     """Merging script with a simple concatenation, no separator"""
     values = [str(v) for v in args if v is not None]
     separator = ""
+
     return separator.join(values)
