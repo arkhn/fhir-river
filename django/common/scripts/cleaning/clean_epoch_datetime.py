87a9161b4dcbb66c8b5e22ece1c832129ae85125
nriss <nicolas.riss22@gmail.com>
2021-09-02 17:23:39 +0200

    fix(epoch_conversion): update return

--- django/common/scripts/cleaning/clean_epoch_datetime.py
+++ django/common/scripts/cleaning/clean_epoch_datetime.py
@@ -1,10 +1,13 @@
 import datetime
-import re
 
 from common.scripts import utils
 
 
 def clean_epoch_dateTime(raw_input):
+
+    if utils.is_empty(raw_input):
+        return ""
+
     # Handle epoch
     try:
         if len(raw_input) < 12:
@@ -12,14 +15,6 @@ def clean_epoch_dateTime(raw_input):
         else:
             date = datetime.datetime.fromtimestamp(float(raw_input) / 1000)
         date_with_tz = date.replace(tzinfo=datetime.timezone(datetime.timedelta(hours=2)))
-        result = date_with_tz.isoformat()
+        return date_with_tz.isoformat()
     except ValueError:
-        pass
-
-    if date is None:
         return raw_input
-
-    if result is None:
-        return raw_input
-
-    return result
