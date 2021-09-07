6312acd00faa72c444ff7d50ebfd21efcd79adae
Teva RIOU <teva.riou@gmail.com>
2021-09-07 17:13:09 +0200

    test(api): fix override settings

--- tests/river/unit/common/test_concept_maps.py
+++ tests/river/unit/common/test_concept_maps.py
@@ -1,15 +1,13 @@
 from pathlib import Path
 
-from django.test import override_settings
-
 from common.adapters.fhir_api import fhir_api
 from river.common.mapping import concept_maps
 
 FIXTURES_DIR = Path(__file__).resolve().parent / "fixtures"
 
 
-@override_settings(DEFAULT_FHIR_API_CLASS="common.adapters.fhir_api.InMemoryFhirAPI")
-def test_format_concept_map(concept_map):
+def test_format_concept_map(settings, concept_map):
+    settings.DEFAULT_FHIR_API_CLASS = "common.adapters.fhir_api.InMemoryFhirAPI"
     fhir_api.create("ConceptMap", concept_map)
 
     actual = concept_maps.format_concept_map("id", "validToken")
@@ -20,8 +18,8 @@ def test_format_concept_map(concept_map):
     assert actual == expected
 
 
-@override_settings(DEFAULT_FHIR_API_CLASS="common.adapters.fhir_api.InMemoryFhirAPI")
-def test_dereference_concept_map(concept_map):
+def test_dereference_concept_map(settings, concept_map):
+    settings.DEFAULT_FHIR_API_CLASS = "common.adapters.fhir_api.InMemoryFhirAPI"
     fhir_api.create("ConceptMap", concept_map)
 
     mapping = {"attributes": [{"input_groups": [{"inputs": [{"concept_map_id": "cm_gender"}]}]}]}
