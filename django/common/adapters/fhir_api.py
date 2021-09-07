ad35796e2a99beea1d82a9f42c0dd15bb35144e5
Teva RIOU <teva.riou@gmail.com>
2021-09-07 17:06:51 +0200

    test(api): add fhir_api integration test

--- django/common/adapters/fhir_api.py
+++ django/common/adapters/fhir_api.py
@@ -42,6 +42,16 @@ class HapiFhirAPI(FhirAPI):
         super().__init__()
         self._url = settings.FHIR_API_URL
 
+    def create(self, resource_type: str, payload: dict, auth_token=None):
+        headers = {**self._headers, "Authorization": f"Bearer {auth_token}"} if auth_token else self._headers
+        response = requests.post(
+            f"{self._url}/{resource_type}/",
+            json=payload,
+            headers=headers,
+        )
+        response.raise_for_status()
+        return response.json()
+
     def retrieve(self, resource_type, resource_id, auth_token=None):
         headers = {**self._headers, "Authorization": f"Bearer {auth_token}"} if auth_token else self._headers
 
