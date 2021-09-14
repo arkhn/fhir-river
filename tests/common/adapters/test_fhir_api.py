7a7c3d7fb58943debdebcba042039fae66351576
simonvadee <simon.vadee@gmail.com>
2021-09-14 15:52:28 +0200

    feat(river): use HAPI FHIR $validate in preview

--- tests/common/adapters/test_fhir_api.py
+++ tests/common/adapters/test_fhir_api.py
@@ -4,4 +4,4 @@ from common.adapters.fhir_api import HapiFhirAPI
 def test_fhir_api():
     fhir_api = HapiFhirAPI()
 
-    assert fhir_api._headers == {"Cache-Control": "no-cache"}
+    assert fhir_api._headers == {"Cache-Control": "no-cache", "Content-Type": "application/fhir+json"}
