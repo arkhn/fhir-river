b565926c57d9d1202b715f9aefde65deefef01e9
Klochette <melina.chamayou@hetic.net>
2021-09-14 15:30:09 +0200

    delete accordions and add paper

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -4,6 +4,7 @@ type EnhancedWindow = typeof window & {
     API_URL?: string;
     OIDC_LOGIN_URL?: string;
     CSRF_COOKIE_NAME?: string;
+    KIBANA_URL?: string;
   };
 };
 
@@ -11,6 +12,7 @@ let {
   PUBLIC_URL,
   REACT_APP_API_URL: API_URL,
   REACT_APP_OIDC_LOGIN_URL: OIDC_LOGIN_URL,
+  REACT_APP_KIBANA_URL: KIBANA_URL,
   REACT_APP_CSRF_COOKIE_NAME: CSRF_COOKIE_NAME,
 } = process.env;
 
@@ -20,6 +22,7 @@ if (process.env.NODE_ENV === "production") {
     PUBLIC_URL,
     API_URL,
     OIDC_LOGIN_URL,
+    KIBANA_URL,
     CSRF_COOKIE_NAME,
   } = (window as EnhancedWindow).env);
 }
@@ -29,6 +32,7 @@ const URI_STATIC_VALUE_PREFIX = "http://terminology.arkhn.org/";
 export {
   PUBLIC_URL,
   API_URL,
+  KIBANA_URL,
   OIDC_LOGIN_URL,
   CSRF_COOKIE_NAME,
   URI_STATIC_VALUE_PREFIX,
--- app/src/features/Batches/BatchList.tsx
+++ app/src/features/Batches/BatchList.tsx
@@ -1,90 +1,36 @@
 import React, { useState } from "react";
 
-import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  CircularProgress,
-  Typography,
-} from "@material-ui/core";
+import { CircularProgress, Link, Paper, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { ExpandMore, Replay } from "@material-ui/icons";
+import { Replay } from "@material-ui/icons";
 import Pagination from "@material-ui/lab/Pagination";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
 
 import Button from "common/components/Button";
 import { useApiBatchesListQuery } from "services/api/endpoints";
+import { Batch } from "services/api/generated/api.generated";
 
+import { KIBANA_URL } from "../../constants";
 import BatchCancel from "./BatchCancel";
-import BatchErrors from "./BatchErrors";
 
 const useStyles = makeStyles((theme) => ({
-  accordions: {
+  batchList: {
     overflowY: "scroll",
     margin: theme.spacing(0, 5, 8, 5),
   },
-  accordion: {
-    borderRadius: theme.shape.borderRadius,
+  listItem: {
     marginBottom: theme.spacing(0.5),
-    "&.Mui-expanded": {
-      margin: theme.spacing(0, 0, 0.5, 0),
-    },
-    "&:before": {
-      display: "none",
-    },
-  },
-  accordionSummary: {
-    borderBottom: "none",
-    transition: "none",
-    "&.Mui-expanded": {
-      minHeight: 56,
-      borderBottom: `1px solid ${theme.palette.background.default}`,
-    },
-  },
-  accordionSummaryContent: {
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
-    padding: theme.spacing(1, 0),
+    padding: theme.spacing(1.3, 2, 1, 2),
     flexWrap: "wrap",
-    minHeight: 56,
-    margin: 0,
-    transition: "none",
-    "&.Mui-expanded": {
-      minHeight: 56,
-      margin: 0,
-    },
   },
-  accordionSummaryTitle: {
+  listItemActions: {
     display: "flex",
-    justifyContent: "space-between",
     alignItems: "center",
     flexWrap: "wrap",
-    flexGrow: 1,
-    marginRight: theme.spacing(1),
-  },
-  batchId: {
-    fontSize: theme.typography.subtitle2.fontSize,
-    backgroundColor: theme.palette.divider,
-    padding: theme.spacing(0, 0.5),
-    borderRadius: 3,
-    marginTop: theme.spacing(0.5),
-  },
-  endButtons: {
-    display: "flex",
-    marginTop: "auto",
-    marginBottom: "auto",
-    justifyContent: "start",
-    flexWrap: "wrap",
-  },
-  button: {
-    margin: theme.spacing(0.5, 1, 0.5, 0),
-  },
-  accordionDetails: {
-    display: "flex",
-    flexDirection: "column",
-    marginTop: theme.spacing(0.5, 0, 1, 0),
   },
   paginationContainer: {
     backgroundColor: theme.palette.background.default,
@@ -101,6 +47,12 @@ const useStyles = makeStyles((theme) => ({
   loader: {
     display: "flex",
     justifyContent: "center",
+  },
+  margin: {
+    marginRight: theme.spacing(1),
+  },
+  title: {
+    display: "flex",
   },
 }));
 
@@ -130,6 +82,34 @@ const BatchList = (): JSX.Element => {
     }
   );
 
+  const convertDateToTime = (batchStart: string, batchEnd: string) => {
+    const timeBatchStart = new Date(batchStart).getTime();
+    const timeBatchEnd = new Date(batchEnd).getTime();
+    const duration = timeBatchEnd - timeBatchStart;
+    const hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((duration / (1000 * 60)) % 60);
+    return { hours, minutes };
+  };
+
+  const getBatchDuration = (batch: Batch) => {
+    const batchEnd = batch.completed_at ?? batch.canceled_at;
+    if (batchEnd) {
+      const { hours, minutes } = convertDateToTime(batch.created_at, batchEnd);
+      if (hours > 0 && minutes > 0) {
+        return `${t("hour", { count: hours })} ${t("minute", {
+          count: minutes,
+        })}`;
+      } else if (hours > 0 && minutes === 0) {
+        return t("hour", { count: hours });
+      } else {
+        return t("minute", { count: minutes });
+      }
+    }
+  };
+
+  const getCreatedAtDate = (date: string) =>
+    new Date(date).toLocaleString().split(",").join(" -");
+
   const handleBatchRetry = (batchId: string) => (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
@@ -146,65 +126,67 @@ const BatchList = (): JSX.Element => {
     );
   return (
     <>
-      <div className={classes.accordions}>
+      <div className={classes.batchList}>
         {batches?.results &&
           batches.results.map((batch) => (
-            <Accordion className={classes.accordion} key={`batch-${batch.id}`}>
-              <AccordionSummary
-                classes={{
-                  root: classes.accordionSummary,
-                  content: classes.accordionSummaryContent,
-                }}
-                expandIcon={<ExpandMore />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-              >
-                <div className={classes.accordionSummaryTitle}>
-                  <div>
-                    <Typography variant="subtitle2">
-                      {!batch.completed_at &&
-                        !batch.canceled_at &&
-                        t("batchInProgress")}
-                      {batch.completed_at &&
-                        batch.errors.length > 0 &&
-                        t("batchErrors", {
-                          errors: batch.errors.length.toString(),
-                        })}
-
-                      {batch.completed_at &&
-                        batch.errors.length === 0 &&
-                        t("batchSuccess")}
-                      {batch.canceled_at && t("batchCanceled")}
-                    </Typography>
-                    <Typography className={classes.batchId}>
-                      {batch.id}
-                    </Typography>
-                  </div>
+            <Paper
+              key={batch.id}
+              className={classes.listItem}
+              variant="outlined"
+              elevation={2}
+            >
+              <div>
+                <div className={classes.title}>
                   <Typography variant="subtitle2">
-                    {new Date(batch.created_at)
-                      .toLocaleString()
-                      .split(",")
-                      .join(" -")}
+                    {!batch.completed_at &&
+                      !batch.canceled_at &&
+                      t("batchInProgress")}
+                    {batch.completed_at && batch.errors.length > 0 && (
+                      <>
+                        {t("batchErrors", {
+                          count: batch.errors.length,
+                        })}{" "}
+                        <Link
+                          target="_blank"
+                          rel="noopener"
+                          href={`${KIBANA_URL}app/kibana#/discover?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1y,to:now))&_a=(columns:!(_source),filters:!(),interval:auto,query:(language:kuery,query:${batch.id}),sort:!())`}
+                        >
+                          ({t("seeOnKibana")})
+                        </Link>
+                      </>
+                    )}
+
+                    {batch.completed_at &&
+                      batch.errors.length === 0 &&
+                      t("batchSuccess")}
+                    {batch.canceled_at && t("batchCanceled")}
                   </Typography>
                 </div>
-                <div className={classes.endButtons}>
-                  <Button
-                    className={classes.button}
-                    variant="outlined"
-                    startIcon={<Replay />}
-                    onClick={handleBatchRetry(batch.id)}
-                  >
-                    {t("retry")}
-                  </Button>
-                  {!batch.canceled_at && !batch.completed_at && (
-                    <BatchCancel batch={batch} className={classes.button} />
-                  )}
-                </div>
-              </AccordionSummary>
-              <AccordionDetails className={classes.accordionDetails}>
-                <BatchErrors batch={batch} />
-              </AccordionDetails>
-            </Accordion>
+                {(batch.completed_at || batch.canceled_at) && (
+                  <Typography variant="body2" color="textSecondary">
+                    {`${getBatchDuration(batch)} | ${t("resource", {
+                      count: batch.resources.length,
+                    })}`}
+                  </Typography>
+                )}
+              </div>
+              <div className={classes.listItemActions}>
+                <Typography variant="subtitle2" className={classes.margin}>
+                  {getCreatedAtDate(batch.created_at)}
+                </Typography>
+                <Button
+                  variant="outlined"
+                  startIcon={<Replay />}
+                  onClick={handleBatchRetry(batch.id)}
+                  className={classes.margin}
+                >
+                  {t("retry")}
+                </Button>
+                {!batch.canceled_at && !batch.completed_at && (
+                  <BatchCancel batch={batch} />
+                )}
+              </div>
+            </Paper>
           ))}
       </div>
       <div className={classes.paginationContainer}>
