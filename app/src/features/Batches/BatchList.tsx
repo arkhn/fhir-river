05c827b7a35765a9101d63b9f2676b4a7841aa16
Klochette <melina.chamayou@hetic.net>
2021-09-15 13:38:40 +0200

    delete mockup

--- app/src/features/Batches/BatchList.tsx
+++ app/src/features/Batches/BatchList.tsx
@@ -10,10 +10,7 @@ import { useParams } from "react-router-dom";
 
 import Button from "common/components/Button";
 import { useApiBatchesListQuery } from "services/api/endpoints";
-import {
-  ApiBatchesListApiResponse,
-  Batch,
-} from "services/api/generated/api.generated";
+import { Batch } from "services/api/generated/api.generated";
 
 import { KIBANA_URL } from "../../constants";
 import BatchCancel from "./BatchCancel";
@@ -62,88 +59,6 @@ const useStyles = makeStyles((theme) => ({
 
 const PAGE_SIZE = 10;
 
-const batches: ApiBatchesListApiResponse = {
-  count: 5,
-  next:
-    "http://localhost:8000/api/batches/?limit=10&offset=10&ordering=-created_at&source=ckssxtgmk00080ro4bdw44gqe",
-  previous: null,
-  results: [
-    {
-      canceled_at: null,
-      completed_at: null,
-      created_at: "2021-09-10T12:26:49.957411+02:00",
-      errors: [],
-      id: "ckte7t2p000090qt6uimwv7k6",
-      resources: ["ckssyuqfp00080so06hm7l173"],
-      updated_at: "2021-09-10T12:26:49.957471+02:00",
-    },
-    {
-      canceled_at: "2021-09-10T14:37:51.957411+02:00",
-      completed_at: null,
-      created_at: "2021-09-10T12:26:49.957411+02:00",
-      errors: [],
-      id: "ckte7t2p000090qt6uimwv7k7",
-      resources: ["ckssyuqfp00080so06hm7l173"],
-      updated_at: "2021-09-10T12:26:49.957471+02:00",
-    },
-    {
-      canceled_at: null,
-      completed_at: "2021-09-10T12:33:50.957411+02:00",
-      created_at: "2021-09-10T12:26:49.957411+02:00",
-      errors: [],
-      id: "ckte7t2p000090qt6uimwv7k8",
-      resources: ["ckssyuqfp00080so06hm7l173"],
-      updated_at: "2021-09-10T12:26:49.957471+02:00",
-    },
-    {
-      canceled_at: null,
-      completed_at: "2021-09-15T12:26:50.957411+02:00",
-      created_at: "2021-09-10T10:59:32.000000+02:00",
-      errors: [
-        {
-          id: "1",
-          event: "string",
-          created_at: "string",
-          updated_at: "string",
-          batch: "string",
-        },
-      ],
-      id: "ckte7t2p000090qt6uimwv7k9",
-      resources: [
-        "ckssyuqfp00080so06hm7l173",
-        "ckssyuqfp00080so06hm7l173",
-        "ckssyuqfp00080so06hm7l173",
-        "ckssyuqfp00080so06hm7l173",
-      ],
-      updated_at: "2021-09-10T12:26:49.957471+02:00",
-    },
-    {
-      canceled_at: null,
-      completed_at: "2021-09-11T22:28:50.957411+02:00",
-      created_at: "2021-08-11T21:27:50.957411+02:00",
-      errors: [
-        {
-          id: "1",
-          event: "string",
-          created_at: "string",
-          updated_at: "string",
-          batch: "string",
-        },
-        {
-          id: "2",
-          event: "string",
-          created_at: "string",
-          updated_at: "string",
-          batch: "string",
-        },
-      ],
-      id: "ckt4ia0jn00000lkkh2b9rrh9",
-      resources: ["ckssyuqfp00080so06hm7l173", "ckssyuqfp00080so06hm7l173"],
-      updated_at: "2021-09-10T12:26:49.957471+02:00",
-    },
-  ],
-};
-
 const createKibanaLink = (batchCreation: string) => {
   return `${KIBANA_URL}app/kibana#/discover?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1y,to:now))&_a=(columns:!(_source),filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:ffb9f770-148b-11ec-afc5-23ae59245f97,key:'@timestamp',negate:!f,params:(query:'${batchCreation}'),type:phrase),query:(match_phrase:('@timestamp':'${batchCreation}')))),index:ffb9f770-148b-11ec-afc5-23ae59245f97,interval:auto,query:(language:kuery,query:''),sort:!())`;
 };
@@ -163,14 +78,14 @@ const BatchList = (): JSX.Element => {
 
   const offset = (page - 1) * PAGE_SIZE;
   const limit = offset + PAGE_SIZE;
-  const {
-    /* data: batches, */ isLoading: isBatchesLoading,
-  } = useApiBatchesListQuery({
-    limit,
-    offset,
-    source: [sourceId],
-    ordering: "-created_at",
-  });
+  const { data: batches, isLoading: isBatchesLoading } = useApiBatchesListQuery(
+    {
+      limit,
+      offset,
+      source: [sourceId],
+      ordering: "-created_at",
+    }
+  );
 
   const getDurationString = (duration: number, unit: string) => {
     if (duration) return `${t(`${unit}`, { count: duration })} `;
