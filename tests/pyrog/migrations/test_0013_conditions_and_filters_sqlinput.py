90f4e268b124cd98728d599dc15bc65d946083e5
simonvadee <simon.vadee@gmail.com>
2021-09-17 17:57:37 +0200

    clean(tests): refactor migration tests

--- tests/pyrog/migrations/test_0013_conditions_and_filters_sqlinput.py
+++ tests/pyrog/migrations/test_0013_conditions_and_filters_sqlinput.py
@@ -5,18 +5,13 @@ from django.conf import settings
 pytestmark = pytest.mark.django_db
 
 
-@pytest.mark.migration(app_label="pyrog", migration_name="0012_drop_input")
-def test_migrate(migrator, state):
-    # instantiate all objects required for Condition and Filter
+def create_input_group(state):
     Owner = state.apps.get_model("pyrog.Owner")
     InputGroup = state.apps.get_model("pyrog.InputGroup")
-    Column = state.apps.get_model("pyrog.Column")
     Resource = state.apps.get_model("pyrog.Resource")
     Attribute = state.apps.get_model("pyrog.Attribute")
     Source = state.apps.get_model("pyrog.Source")
     Credential = state.apps.get_model("pyrog.Credential")
-    Condition = state.apps.get_model("pyrog", "Condition")
-    Filter = state.apps.get_model("pyrog", "Filter")
 
     source = Source.objects.create(name="source")
     credential = Credential.objects.create(
@@ -32,10 +27,23 @@ def test_migrate(migrator, state):
     resource = Resource.objects.create(primary_key_owner=owner, source=source)
     attribute = Attribute.objects.create(resource=resource)
     input_group = InputGroup.objects.create(attribute=attribute)
-    column_1 = Column.objects.create(owner=owner, table="table", column="col1")
-    column_2 = Column.objects.create(owner=owner, table="table", column="col2")
+    return input_group
+
+
+@pytest.mark.migration(app_label="pyrog", migration_name="0012_drop_input")
+def test_migrate(migrator, state):
+    Owner = state.apps.get_model("pyrog.Owner")
+    Resource = state.apps.get_model("pyrog.Resource")
+    Column = state.apps.get_model("pyrog.Column")
+    Condition = state.apps.get_model("pyrog", "Condition")
+    Filter = state.apps.get_model("pyrog", "Filter")
+
+    input_group = create_input_group(state)
+
+    column_1 = Column.objects.create(owner=Owner.objects.first(), table="table", column="col1")
+    column_2 = Column.objects.create(owner=Owner.objects.first(), table="table", column="col2")
     condition = Condition.objects.create(input_group=input_group, column=column_1)
-    filter = Filter.objects.create(sql_column=column_2, resource=resource)
+    filter = Filter.objects.create(sql_column=column_2, resource=Resource.objects.first())
 
     new_state = migrator.apply_tested_migration(("pyrog", "0013_conditions_and_filters_sqlinput"))
 
@@ -50,36 +58,19 @@ def test_migrate(migrator, state):
 @pytest.mark.migration(app_label="pyrog", migration_name="0013_conditions_and_filters_sqlinput")
 def test_migrate_reverse(migrator, state):
     Owner = state.apps.get_model("pyrog.Owner")
-    InputGroup = state.apps.get_model("pyrog.InputGroup")
-    Column = state.apps.get_model("pyrog.Column")
-    SQLInput = state.apps.get_model("pyrog.SQLInput")
     Resource = state.apps.get_model("pyrog.Resource")
-    Attribute = state.apps.get_model("pyrog.Attribute")
-    Source = state.apps.get_model("pyrog.Source")
-    Credential = state.apps.get_model("pyrog.Credential")
+    Column = state.apps.get_model("pyrog.Column")
     Condition = state.apps.get_model("pyrog", "Condition")
     Filter = state.apps.get_model("pyrog", "Filter")
+    SQLInput = state.apps.get_model("pyrog", "SQLInput")
 
-    source = Source.objects.create(name="source")
-    credential = Credential.objects.create(
-        source=source,
-        host=settings.DATABASES["default"]["HOST"],
-        port=settings.DATABASES["default"]["PORT"],
-        database=settings.DATABASES["default"]["NAME"],
-        login=settings.DATABASES["default"]["USER"],
-        password=settings.DATABASES["default"]["PASSWORD"],
-        model="POSTGRES",
-    )
-    owner = Owner.objects.create(name="owner", credential=credential)
-    resource = Resource.objects.create(primary_key_owner=owner, source=source)
-    attribute = Attribute.objects.create(resource=resource)
-    input_group = InputGroup.objects.create(attribute=attribute)
-    column_1 = Column.objects.create(owner=owner, table="table", column="col1")
-    column_2 = Column.objects.create(owner=owner, table="table", column="col2")
+    input_group = create_input_group(state)
+    column_1 = Column.objects.create(owner=Owner.objects.first(), table="table", column="col1")
+    column_2 = Column.objects.create(owner=Owner.objects.first(), table="table", column="col2")
     sql_input_1 = SQLInput.objects.create(input_group=input_group, column=column_1)
     sql_input_2 = SQLInput.objects.create(input_group=input_group, column=column_2)
     condition = Condition.objects.create(input_group=input_group, sql_input=sql_input_1)
-    filter = Filter.objects.create(sql_input=sql_input_2, resource=resource)
+    filter = Filter.objects.create(sql_input=sql_input_2, resource=Resource.objects.first())
 
     assert len(SQLInput.objects.all()) == 2
     condition_input = SQLInput.objects.get(condition=condition.id)
--- tests/pyrog/migrations/test_0014_split_join_left_right.py
+++ tests/pyrog/migrations/test_0014_split_join_left_right.py
@@ -5,8 +5,7 @@ from django.conf import settings
 pytestmark = pytest.mark.django_db
 
 
-@pytest.mark.migration(app_label="pyrog", migration_name="0013_conditions_and_filters_sqlinput")
-def test_migrate(migrator, state):
+def create_sql_input(state):
     Credential = state.apps.get_model("pyrog.Credential")
     Owner = state.apps.get_model("pyrog.Owner")
     InputGroup = state.apps.get_model("pyrog.InputGroup")
@@ -14,7 +13,6 @@ def test_migrate(migrator, state):
     Resource = state.apps.get_model("pyrog.Resource")
     Attribute = state.apps.get_model("pyrog.Attribute")
     Source = state.apps.get_model("pyrog.Source")
-    Join = state.apps.get_model("pyrog", "Join")
     Column = state.apps.get_model("pyrog", "Column")
 
     source = Source.objects.create(name="source")
@@ -33,9 +31,20 @@ def test_migrate(migrator, state):
     input_group = InputGroup.objects.create(attribute=attribute)
     base_col = Column.objects.create(owner=owner, table="table", column="col1")
     sql_input = SQLInput.objects.create(input_group=input_group, column=base_col)
-    join = Join.objects.create(column=base_col)
-    left_col = Column.objects.create(owner=owner, table="table", column="col_left", join=join)
-    right_col = Column.objects.create(owner=owner, table="table", column="col_right", join=join)
+    return sql_input
+
+
+@pytest.mark.migration(app_label="pyrog", migration_name="0013_conditions_and_filters_sqlinput")
+def test_migrate(migrator, state):
+    Owner = state.apps.get_model("pyrog.Owner")
+    Join = state.apps.get_model("pyrog", "Join")
+    Column = state.apps.get_model("pyrog", "Column")
+
+    sql_input = create_sql_input(state)
+
+    join = Join.objects.create(column=sql_input.column)
+    left_col = Column.objects.create(owner=Owner.objects.first(), table="table", column="col_left", join=join)
+    right_col = Column.objects.create(owner=Owner.objects.first(), table="table", column="col_right", join=join)
 
     new_state = migrator.apply_tested_migration(("pyrog", "0014_split_join_left_right"))
 
@@ -49,34 +58,14 @@ def test_migrate(migrator, state):
 
 @pytest.mark.migration(app_label="pyrog", migration_name="0014_split_join_left_right")
 def test_migrate_reverse(migrator, state):
-    Credential = state.apps.get_model("pyrog.Credential")
     Owner = state.apps.get_model("pyrog.Owner")
-    InputGroup = state.apps.get_model("pyrog.InputGroup")
-    SQLInput = state.apps.get_model("pyrog.SQLInput")
-    Resource = state.apps.get_model("pyrog.Resource")
-    Attribute = state.apps.get_model("pyrog.Attribute")
-    Source = state.apps.get_model("pyrog.Source")
     Join = state.apps.get_model("pyrog", "Join")
     Column = state.apps.get_model("pyrog", "Column")
 
-    source = Source.objects.create(name="source")
-    credential = Credential.objects.create(
-        source=source,
-        host=settings.DATABASES["default"]["HOST"],
-        port=settings.DATABASES["default"]["PORT"],
-        database=settings.DATABASES["default"]["NAME"],
-        login=settings.DATABASES["default"]["USER"],
-        password=settings.DATABASES["default"]["PASSWORD"],
-        model="POSTGRES",
-    )
-    owner = Owner.objects.create(name="owner", credential=credential)
-    resource = Resource.objects.create(primary_key_owner=owner, source=source)
-    attribute = Attribute.objects.create(resource=resource)
-    input_group = InputGroup.objects.create(attribute=attribute)
-    base_col = Column.objects.create(owner=owner, table="table", column="col1")
-    sql_input = SQLInput.objects.create(input_group=input_group, column=base_col)
-    left_col = Column.objects.create(owner=owner, table="table", column="col_left")
-    right_col = Column.objects.create(owner=owner, table="table", column="col_right")
+    sql_input = create_sql_input(state)
+
+    left_col = Column.objects.create(owner=Owner.objects.first(), table="table", column="col_left")
+    right_col = Column.objects.create(owner=Owner.objects.first(), table="table", column="col_right")
     join = Join.objects.create(sql_input=sql_input, left=left_col, right=right_col)
 
     new_state = migrator.apply_tested_migration(("pyrog", "0013_conditions_and_filters_sqlinput"))
