35b29fa41ba6e23138216818793119c6493913e0
Jasopaum <jason.paumier@gmail.com>
2021-09-20 18:22:41 +0200

    chore(tests): use real mimic db instead of mocks in test_query_builder

--- tests/river/unit/extractor/extract/test_query_builder.py
+++ tests/river/unit/extractor/extract/test_query_builder.py
@@ -8,66 +8,16 @@ from river.common.analyzer.input_group import InputGroup
 from river.common.analyzer.sql_column import SqlColumn
 from river.common.analyzer.sql_filter import SqlFilter
 from river.common.analyzer.sql_join import SqlJoin
+from river.common.database_connection.db_connection import DBConnection
 from river.extractor.query_builder import QueryBuilder
-from sqlalchemy import Column, MetaData, Table
-from sqlalchemy.orm.query import Query
-
-meta = MetaData()
-tables = {
-    "patients": Table(
-        "patients",
-        meta,
-        Column("subject_id"),
-        Column("row_id"),
-        Column("patient_id"),
-        Column("gender"),
-        Column("dob"),
-        Column("dod"),
-        schema="public",
-    ),
-    "admissions": Table(
-        "admissions",
-        meta,
-        Column("subject_id"),
-        Column("row_id"),
-        Column("patient_id"),
-        Column("admittime"),
-        Column("marital_status"),
-        Column("language"),
-        schema="public",
-    ),
-    "prescriptions": Table("prescriptions", meta, Column("row_id"), schema="public"),
-    "join_table": Table("join_table", meta, Column("pat_id"), Column("adm_id"), schema="public"),
-    "d_items": Table("d_items", meta, Column("itemid"), Column("label"), schema="public"),
-    "chartevents": Table(
-        "chartevents",
-        meta,
-        Column("row_id"),
-        Column("subject_id"),
-        Column("charttime"),
-        Column("icustay_id"),
-        Column("itemid"),
-        Column("valuenum"),
-        schema="public",
-    ),
-}
-
-
-def mock_table(name, metadata, **kwargs):
-    return tables[name]
-
-
-def mock_alchemy_query(*columns):
-    return Query([*columns])
 
 
 def make_query_builder(analysis, pk_values=None):
-    session = mock.MagicMock()
-    session.query = mock_alchemy_query
-    return QueryBuilder(session, None, analysis, pk_values)
+    db_connection = DBConnection(analysis.source_credentials)
+    with db_connection.session_scope() as session:
+        return QueryBuilder(session, db_connection.metadata, analysis, pk_values)
 
 
-@mock.patch("river.extractor.query_builder.Table", mock_table)
 def test_with_real_analysis(mimic_mapping, snapshot):
     analyzer = Analyzer()
     # Patient - feat_6_join
@@ -82,11 +32,18 @@ def test_with_real_analysis(mimic_mapping, snapshot):
 
 
 @mock.patch("river.common.analyzer.sql_column.hashlib.sha1")
-@mock.patch("river.extractor.query_builder.Table", mock_table)
 def test_sqlalchemy_query(mock_sha1):
     mock_sha1.return_value.hexdigest.return_value = "hash"
 
     analysis = Analysis()
+    analysis.source_credentials = {
+        "host": "mimic",
+        "port": 5432,
+        "database": "test",
+        "login": "test",
+        "password": "test",
+        "model": "POSTGRES",
+    }
 
     attribute_a = Attribute(path="path", definition_id="string")
     input_group_a = InputGroup(id_="group", attribute=attribute_a)
@@ -115,8 +72,8 @@ def test_sqlalchemy_query(mock_sha1):
             "admittime",
             joins=[
                 SqlJoin(
-                    SqlColumn("public", "patients", "patient_id"),
-                    SqlColumn("public", "admissions", "patient_id"),
+                    SqlColumn("public", "patients", "subject_id"),
+                    SqlColumn("public", "admissions", "subject_id"),
                 )
             ],
         )
@@ -138,8 +95,8 @@ def test_sqlalchemy_query(mock_sha1):
                 "admittime",
                 joins=[
                     SqlJoin(
-                        SqlColumn("public", "patients", "patient_id"),
-                        SqlColumn("public", "admissions", "patient_id"),
+                        SqlColumn("public", "patients", "subject_id"),
+                        SqlColumn("public", "admissions", "subject_id"),
                     )
                 ],
             ),
@@ -160,18 +117,25 @@ def test_sqlalchemy_query(mock_sha1):
         "admissions_1.admittime AS admissions_admittime_hash \n"
         "FROM public.patients "
         "LEFT OUTER JOIN public.admissions AS admissions_1 "
-        "ON admissions_1.patient_id = public.patients.patient_id \n"
-        "WHERE admissions_1.admittime LIKE :param_1"
+        "ON admissions_1.subject_id = public.patients.subject_id \n"
+        "WHERE admissions_1.admittime LIKE %(param_1)s"
     )
     assert query.statement.compile().params == {"param_1": "2150-08-29"}
 
 
 @mock.patch("river.common.analyzer.sql_column.hashlib.sha1")
-@mock.patch("river.extractor.query_builder.Table", mock_table)
 def test_2hop_joins(mock_sha1):
     mock_sha1.return_value.hexdigest.return_value = "hash"
 
     analysis = Analysis()
+    analysis.source_credentials = {
+        "host": "mimic",
+        "port": 5432,
+        "database": "test",
+        "login": "test",
+        "password": "test",
+        "model": "POSTGRES",
+    }
 
     attribute = Attribute(path="path", definition_id="string")
     input_group = InputGroup(id_="group", attribute=attribute)
@@ -183,12 +147,12 @@ def test_2hop_joins(mock_sha1):
             "admittime",
             joins=[
                 SqlJoin(
-                    SqlColumn("public", "patients", "row_id"),
-                    SqlColumn("public", "join_table", "pat_id"),
+                    SqlColumn("public", "patients", "subject_id"),
+                    SqlColumn("public", "prescriptions", "subject_id"),
                 ),
                 SqlJoin(
-                    SqlColumn("public", "join_table", "adm_id"),
-                    SqlColumn("public", "admissions", "row_id"),
+                    SqlColumn("public", "prescriptions", "hadm_id"),
+                    SqlColumn("public", "admissions", "hadm_id"),
                 ),
             ],
         )
@@ -204,17 +168,25 @@ def test_2hop_joins(mock_sha1):
         "SELECT public.patients.subject_id AS patients_subject_id_hash, "
         "admissions_1.admittime AS admissions_admittime_hash \n"
         "FROM public.patients "
-        "LEFT OUTER JOIN public.join_table AS join_table_1 ON join_table_1.pat_id = public.patients.row_id "
-        "LEFT OUTER JOIN public.admissions AS admissions_1 ON admissions_1.row_id = join_table_1.adm_id"
+        "LEFT OUTER JOIN public.prescriptions AS prescriptions_1 "
+        "ON prescriptions_1.subject_id = public.patients.subject_id "
+        "LEFT OUTER JOIN public.admissions AS admissions_1 ON admissions_1.hadm_id = prescriptions_1.hadm_id"
     )
 
 
 @mock.patch("river.common.analyzer.sql_column.hashlib.sha1")
-@mock.patch("river.extractor.query_builder.Table", mock_table)
 def test_1and2hop_joins(mock_sha1):
     mock_sha1.return_value.hexdigest.return_value = "hash"
 
     analysis = Analysis()
+    analysis.source_credentials = {
+        "host": "mimic",
+        "port": 5432,
+        "database": "test",
+        "login": "test",
+        "password": "test",
+        "model": "POSTGRES",
+    }
 
     attribute = Attribute(path="path", definition_id="string")
     input_group = InputGroup(id_="group", attribute=attribute)
@@ -222,12 +194,12 @@ def test_1and2hop_joins(mock_sha1):
     input_group.add_column(
         SqlColumn(
             "public",
-            "join_table",
-            "adm_id",
+            "prescriptions",
+            "hadm_id",
             joins=[
                 SqlJoin(
-                    SqlColumn("public", "patients", "row_id"),
-                    SqlColumn("public", "join_table", "pat_id"),
+                    SqlColumn("public", "patients", "subject_id"),
+                    SqlColumn("public", "prescriptions", "subject_id"),
                 ),
             ],
         )
@@ -239,12 +211,12 @@ def test_1and2hop_joins(mock_sha1):
             "admittime",
             joins=[
                 SqlJoin(
-                    SqlColumn("public", "patients", "row_id"),
-                    SqlColumn("public", "join_table", "pat_id"),
+                    SqlColumn("public", "patients", "subject_id"),
+                    SqlColumn("public", "prescriptions", "subject_id"),
                 ),
                 SqlJoin(
-                    SqlColumn("public", "join_table", "adm_id"),
-                    SqlColumn("public", "admissions", "row_id"),
+                    SqlColumn("public", "prescriptions", "hadm_id"),
+                    SqlColumn("public", "admissions", "hadm_id"),
                 ),
             ],
         )
@@ -258,20 +230,28 @@ def test_1and2hop_joins(mock_sha1):
 
     assert str(query) == (
         "SELECT public.patients.subject_id AS patients_subject_id_hash, "
-        "join_table_1.adm_id AS join_table_adm_id_hash, "
+        "prescriptions_1.hadm_id AS prescriptions_hadm_id_hash, "
         "admissions_1.admittime AS admissions_admittime_hash \n"
         "FROM public.patients "
-        "LEFT OUTER JOIN public.join_table AS join_table_1 ON join_table_1.pat_id = public.patients.row_id "
-        "LEFT OUTER JOIN public.admissions AS admissions_1 ON admissions_1.row_id = join_table_1.adm_id"
+        "LEFT OUTER JOIN public.prescriptions AS prescriptions_1 "
+        "ON prescriptions_1.subject_id = public.patients.subject_id "
+        "LEFT OUTER JOIN public.admissions AS admissions_1 ON admissions_1.hadm_id = prescriptions_1.hadm_id"
     )
 
 
 @mock.patch("river.common.analyzer.sql_column.hashlib.sha1")
-@mock.patch("river.extractor.query_builder.Table", mock_table)
 def test_duplicated_joins(mock_sha1):
     mock_sha1.return_value.hexdigest.return_value = "hash"
 
     analysis = Analysis()
+    analysis.source_credentials = {
+        "host": "mimic",
+        "port": 5432,
+        "database": "test",
+        "login": "test",
+        "password": "test",
+        "model": "POSTGRES",
+    }
 
     attribute_a = Attribute(path="path", definition_id="string")
     input_group_a = InputGroup(id_="group", attribute=attribute_a)
@@ -345,10 +325,18 @@ def test_duplicated_joins(mock_sha1):
 
 
 @mock.patch("river.common.analyzer.sql_column.hashlib.sha1")
-@mock.patch("river.extractor.query_builder.Table", mock_table)
 def test_apply_filters(mock_sha1):
     mock_sha1.return_value.hexdigest.return_value = "hash"
+
     analysis = Analysis()
+    analysis.source_credentials = {
+        "host": "mimic",
+        "port": 5432,
+        "database": "test",
+        "login": "test",
+        "password": "test",
+        "model": "POSTGRES",
+    }
 
     analysis.primary_key_column = SqlColumn("public", "patients", "subject_id")
     analysis.add_filter(SqlFilter(SqlColumn("public", "admissions", "admittime"), "LIKE", "2150-08-29"))
@@ -363,10 +351,10 @@ def test_apply_filters(mock_sha1):
     assert str(query) == (
         "SELECT public.patients.subject_id AS patients_subject_id_hash \n"
         "FROM public.patients, public.admissions \n"
-        "WHERE public.patients.subject_id IN (:param_1, :param_2) "
-        "AND public.admissions.admittime LIKE :param_3 "
-        "AND public.patients.row_id <= :param_4 "
-        "AND public.patients.row_id >= :param_5 AND public.patients.row_id <= :param_6"
+        "WHERE public.patients.subject_id IN (%(param_1)s, %(param_2)s) "
+        "AND public.admissions.admittime LIKE %(param_3)s "
+        "AND public.patients.row_id <= %(param_4)s "
+        "AND public.patients.row_id >= %(param_5)s AND public.patients.row_id <= %(param_6)s"
     )
     assert query.statement.compile().params == {
         "param_1": 123,
@@ -379,10 +367,19 @@ def test_apply_filters(mock_sha1):
 
 
 @mock.patch("river.common.analyzer.sql_column.hashlib.sha1")
-@mock.patch("river.extractor.query_builder.Table", mock_table)
 def test_apply_filters_single_value(mock_sha1):
     mock_sha1.return_value.hexdigest.return_value = "hash"
+
     analysis = Analysis()
+    analysis.source_credentials = {
+        "host": "mimic",
+        "port": 5432,
+        "database": "test",
+        "login": "test",
+        "password": "test",
+        "model": "POSTGRES",
+    }
+
     analysis.primary_key_column = SqlColumn("public", "patients", "subject_id")
 
     primary_key = 123
@@ -394,16 +391,24 @@ def test_apply_filters_single_value(mock_sha1):
     assert str(query) == (
         "SELECT public.patients.subject_id AS patients_subject_id_hash \n"
         "FROM public.patients \n"
-        "WHERE public.patients.subject_id = :param_1"
+        "WHERE public.patients.subject_id = %(param_1)s"
     )
     assert query.statement.compile().params == {"param_1": 123}
 
 
 @mock.patch("river.common.analyzer.sql_column.hashlib.sha1")
-@mock.patch("river.extractor.query_builder.Table", mock_table)
 def test_filters_with_joins(mock_sha1):
     mock_sha1.return_value.hexdigest.return_value = "hash"
+
     analysis = Analysis()
+    analysis.source_credentials = {
+        "host": "mimic",
+        "port": 5432,
+        "database": "test",
+        "login": "test",
+        "password": "test",
+        "model": "POSTGRES",
+    }
 
     analysis.primary_key_column = SqlColumn("public", "patients", "subject_id")
     analysis.add_filter(
@@ -431,17 +436,24 @@ def test_filters_with_joins(mock_sha1):
         "SELECT public.patients.subject_id AS patients_subject_id_hash \n"
         "FROM public.patients LEFT OUTER JOIN public.admissions AS admissions_1 "
         "ON admissions_1.subject_id = public.patients.subject_id \n"
-        "WHERE admissions_1.admittime LIKE :param_1"
+        "WHERE admissions_1.admittime LIKE %(param_1)s"
     )
     assert query.statement.compile().params == {"param_1": "2150-08-29"}
 
 
 @mock.patch("river.common.analyzer.sql_column.hashlib.sha1")
-@mock.patch("river.extractor.query_builder.Table", mock_table)
 def test_conditions_with_joins(mock_sha1):
     mock_sha1.return_value.hexdigest.return_value = "hash"
 
     analysis = Analysis()
+    analysis.source_credentials = {
+        "host": "mimic",
+        "port": 5432,
+        "database": "test",
+        "login": "test",
+        "password": "test",
+        "model": "POSTGRES",
+    }
 
     attribute = Attribute(path="path", definition_id="string")
     input_group = InputGroup(id_="group", attribute=attribute)
