25b99cf2b557916fb774eb50e8df38ed785f880c
BPierrick <pierrick.bellut95@gmail.com>
2021-10-05 16:14:27 +0200

    clean: refacto function call

--- app/src/features/Batches/BatchResourceDialog.tsx
+++ app/src/features/Batches/BatchResourceDialog.tsx
@@ -157,8 +157,7 @@ const BatchResourceDialog = ({
 
     if (selectedResourceIds.length > 0) {
       batchCreate();
-      onClose();
-      setSelectedResourceIds([]);
+      onCloseModal();
     }
   };
 
