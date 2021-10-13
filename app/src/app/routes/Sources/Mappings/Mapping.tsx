50a0335e5df7de657449986e61c0d4f4dc8cadeb
Teva RIOU <teva.riou@gmail.com>
2021-10-13 13:14:02 +0200

    fix(app): bad import

--- app/src/app/routes/Sources/Mappings/Mapping.tsx
+++ app/src/app/routes/Sources/Mappings/Mapping.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core";
 import clsx from "clsx";
 
-import Attribute from "features/FhirAttributePanel/FhirAttributePanel";
+import Attribute from "features/Attributes/Attribute";
 import FhirResourceTree from "features/FhirResourceTree/FhirResourceTree";
 
 import MappingHeader from "./MappingHeader";
