a76526193e9fbf9f3f3b4a43a1e633b3ff3c20cb
simonvadee <simon.vadee@gmail.com>
2021-10-14 11:15:54 +0200

    fix(auth): typo in docs

--- django/utils/auth.py
+++ django/utils/auth.py
@@ -5,7 +5,7 @@ import requests
 
 def logout(request):
     """When logging out of river, the session is destroyed in the river app
-    but we also need to destroy he session in oprovider, otherwise the client
+    but we also need to destroy the session in oprovider, otherwise the client
     will simply silently re-login. We therefore call the logout route of oprovider.
     See https://mozilla-django-oidc.readthedocs.io/en/stable/installation.html
     log-user-out-of-the-openid-connect-provider
