a11a5ca86ccbcf7b21cee09bb991f8cd75da9e33
Klochette <melina@arkhn.com>
2021-10-26 20:28:23 +0200

    fix(app): add props to middleware

--- app/src/app/middlewares.ts
+++ app/src/app/middlewares.ts
@@ -1,21 +1,54 @@
 import { isRejectedWithValue, Middleware } from "@reduxjs/toolkit";
 
+import { ApiValidationError } from "services/api/errors";
+
 const rtkQueryErrorLogger: Middleware = () => (next) => (action) => {
   if (isRejectedWithValue(action)) {
-    if (action.payload.data.issue) {
-      action.payload.data.issue.forEach((issue: any) =>
-        console.log(
-          action.payload.status,
-          action.meta.arg.endpointName,
-          issue.diagnostics
-        )
+    const { payload: error } = action;
+    const { data: errorData, status: errorStatus } = error;
+    const { endpointName } = action.meta.arg;
+    console.log(endpointName);
+
+    if (endpointName === "apiStructureDefinitionRetrieve") {
+      errorData.issue.forEach((issue: any) =>
+        console.log(errorStatus, issue.diagnostics)
       );
-    } else
-      console.log(
-        action.payload.status,
-        action.meta.arg.endpointName,
-        action.payload.data.detail
-      );
+    } else if (
+      endpointName === "apiResourcesRetrieve" ||
+      endpointName === "apiAttributesRetrieve"
+    ) {
+      if (errorStatus === "PARSING_ERROR") {
+        console.log(error.originalStatus, errorStatus, error.error);
+      } else if (
+        errorStatus === "FETCH_ERROR" ||
+        errorStatus === "CUSTOM_ERROR"
+      ) {
+        console.log(errorStatus, error.error);
+      } else {
+        console.log(errorStatus, errorData.detail);
+      }
+    } else if (endpointName === "apiStaticInputsUpdate") {
+      console.log(errorStatus, errorData.detail);
+    } else if (
+      endpointName === "apiAttributesCreate" ||
+      endpointName === "apiAttributesList" ||
+      endpointName === "apiInputGroupsList" ||
+      endpointName === "apiInputGroupsCreate" ||
+      endpointName === "apiInputGroupsUpdate" ||
+      endpointName === "apiConditionsList" ||
+      endpointName === "apiConditionsCreate" ||
+      endpointName === "apiColumnsCreate" ||
+      endpointName === "apiStaticInputsCreate"
+    ) {
+      const errors: ApiValidationError<string[]> = errorData;
+      const errorEntries = Object.entries(errors);
+
+      errorEntries.forEach(([key, text]) => {
+        if (text) console.log(errorStatus, key, text.toString());
+      });
+    } else {
+      console.log(action);
+    }
   }
   return next(action);
 };
